5ee2cc031e46444a32f1e36f9f5f2c1d1e7e35b9
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-01-21 10:33:21 +0000

    Quick push while there is no API available

--- server/data/prisonerContactRegistryApiClient.ts
+++ server/data/prisonerContactRegistryApiClient.ts
@@ -1,3 +1,4 @@
+/* eslint @typescript-eslint/no-unused-vars: "off" */
 import { URLSearchParams } from 'url'
 import RestClient from './restClient'
 import { Contact } from '../@types/bapv'
@@ -14,12 +15,71 @@ class PrisonerContactRegistryApiClient {
   constructor(private readonly restclient: RestClient) {}
 
   getPrisonerSocialContacts(offenderNo: string): Promise<Contact[]> {
-    return this.restclient.get({
-      path: `/prisoners/${offenderNo}/contacts`,
-      query: new URLSearchParams({
-        type: 'S',
-      }).toString(),
-    })
+    return Promise.resolve([
+      {
+        personId: 5871791,
+        firstName: 'John',
+        middleName: 'Mark',
+        lastName: 'Smith',
+        dateOfBirth: '1980-01-28',
+        relationshipCode: 'RO',
+        relationshipDescription: 'Responsible Officer',
+        contactType: 'O',
+        contactTypeDescription: 'Official',
+        approvedVisitor: true,
+        emergencyContact: true,
+        nextOfKin: true,
+        restrictions: [
+          {
+            restrictionType: '123',
+            restrictionTypeDescription: '123',
+            startDate: '2000-10-31',
+            expiryDate: '2000-10-31',
+            globalRestriction: true,
+            comment: 'This is a comment text',
+          },
+        ],
+        addresses: [
+          {
+            addressType: 'BUS',
+            flat: '3B',
+            premise: 'Liverpool Prison',
+            street: 'Slinn Street',
+            locality: 'Brincliffe',
+            town: 'Liverpool',
+            postalCode: 'LI1 5TH',
+            county: 'HEREFORD',
+            country: 'ENG',
+            comment: 'This is a comment text',
+            primary: false,
+            noFixedAddress: false,
+            startDate: '2000-10-31',
+            endDate: '2000-10-31',
+            phones: [
+              {
+                number: '0114 2345678',
+                type: 'TEL',
+                ext: '123',
+              },
+            ],
+            addressUsages: [
+              {
+                addressUsage: 'HDC',
+                addressUsageDescription: 'HDC Address',
+                activeFlag: true,
+              },
+            ],
+          },
+        ],
+        commentText: 'This is a comment text',
+      },
+    ])
+    // return this.restclient.get({
+    //   path: `/prisoners/${offenderNo}/contacts`,
+    //   query: new URLSearchParams({
+    //     type: 'S',
+    //   }).toString(),
+    // })
   }
 }
 
