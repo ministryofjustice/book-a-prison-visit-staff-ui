7f8250a328b124d619a6ddb39b6f8b182f786ae7
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2022-02-15 10:16:37 +0000

    Add basic test for homepage cards

---
+++ server/views/pages/index.test.ts
@@ -0,0 +1,29 @@
+import fs from 'fs'
+import cheerio from 'cheerio'
+import nunjucks, { Template } from 'nunjucks'
+import { registerNunjucks } from '../../utils/nunjucksSetup'
+
+const template = fs.readFileSync('server/views/pages/index.njk')
+
+describe('Views - Home', () => {
+  let compiledTemplate: Template
+  let viewContext: Record<string, unknown>
+
+  const njkEnv = registerNunjucks()
+
+  beforeEach(() => {
+    compiledTemplate = nunjucks.compile(template.toString(), njkEnv)
+    viewContext = {}
+  })
+
+  it('should display the home page cards', () => {
+    viewContext = {}
+
+    const $ = cheerio.load(compiledTemplate.render(viewContext))
+
+    expect($('[data-test="book-visit"] .card__link').text()).toEqual('Book a prison visit')
+    expect($('[data-test="book-visit"] .card__link').attr('href')).toEqual('/search')
+
+    expect($('[data-test="find-visit"] .card__link').text()).toEqual('Find a booked visit')
+  })
+})
