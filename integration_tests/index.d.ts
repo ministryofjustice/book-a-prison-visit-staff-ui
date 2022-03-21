93e0d800bb78919f63b7c48316333a5c2fdc1956
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2022-03-21 09:28:36 +0000

    Fix docker caching as per template app pull 74 (#121)

--- integration_tests/index.d.ts
+++ integration_tests/index.d.ts
@@ -3,6 +3,6 @@ declare namespace Cypress {
     /**   * Custom command to signIn. Set failOnStatusCode to false if you expect and non 200 return code
      * @example cy.signIn({ failOnStatusCode: boolean })
      */
-    signIn<S = unknown>(options?: { failOnStatusCode: false }): Chainable<S>
+    signIn(options?: { failOnStatusCode: boolean }): Chainable<AUTWindow>
   }
 }
