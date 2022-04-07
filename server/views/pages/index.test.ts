c9111c37155e089b116b46df0418cdf1a69d759e
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-04-07 13:07:32 +0100

    VB-547: Add cancel a visit tile

--- server/views/pages/index.test.ts
+++ server/views/pages/index.test.ts
@@ -21,9 +21,10 @@ describe('Views - Home', () => {
 
     const $ = cheerio.load(compiledTemplate.render(viewContext))
 
-    expect($('[data-test="book-visit"] .card__link').text()).toEqual('Book a prison visit')
+    expect($('[data-test="book-visit"] .card__link').text()).toEqual('Book a visit')
     expect($('[data-test="book-visit"] .card__link').attr('href')).toEqual('/search')
 
-    expect($('[data-test="find-visit"] .card__link').text()).toEqual('Find a booked visit')
+    expect($('[data-test="cancel-visit"] .card__link').text()).toEqual('Cancel a visit')
+    expect($('[data-test="cancel-visit"] .card__link').attr('href')).toEqual('/cancel')
   })
 })
