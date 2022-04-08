9931c97221c57d005d76cfdb9bf6b9d8ad093b29
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-04-08 08:43:12 +0100

    VB-639: Refactor search pages

--- server/routes/search.test.ts
+++ server/routes/search.test.ts
@@ -54,135 +54,137 @@ afterEach(() => {
   jest.resetAllMocks()
 })
 
-describe('GET /search/', () => {
-  it('should render search page', () => {
-    return request(app)
-      .get('/search/')
-      .expect('Content-Type', /html/)
-      .expect(res => {
-        expect(res.text).toContain('Search for a prisoner')
-      })
-  })
-})
-
-describe('GET /search/results?search=A1234BC', () => {
-  it('should render results page with no results', () => {
-    returnData = {
-      results: [],
-      numberOfResults: 0,
-      numberOfPages: 0,
-      next: 0,
-      previous: 0,
-    }
-
-    return request(app)
-      .get('/search/results?search=A1234BC')
-      .expect('Content-Type', /html/)
-      .expect(res => {
-        expect(res.text).toContain('Search for a prisoner')
-        expect(res.text).toContain('id="search-results-none"')
-      })
-  })
-})
-
-describe('GET /search/results?search=A1234BC', () => {
-  it('should render results page with results and no next/prev when there are less than 11 results', () => {
-    returnData = {
-      results: [
-        [
-          { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
-          { html: 'A1234BC', classes: '' },
-          { html: '2 April 1975', classes: '' },
-        ],
-      ],
-      numberOfPages: 1,
-      numberOfResults: 1,
-      next: 1,
-      previous: 1,
-    }
-
-    return request(app)
-      .get('/search/results?search=A1234BC')
-      .expect('Content-Type', /html/)
-      .expect(res => {
-        expect(res.text).toContain('Search for a prisoner')
-        expect(res.text).toContain('id="search-results-true"')
-        expect(res.text).not.toContain('<p class="moj-pagination__results">')
-      })
+describe('Prisoner search page', () => {
+  describe('GET /search/prisoner', () => {
+    it('should render prisoner search page', () => {
+      return request(app)
+        .get('/search/prisoner')
+        .expect('Content-Type', /html/)
+        .expect(res => {
+          expect(res.text).toContain('Search for a prisoner')
+        })
+    })
   })
 
-  it('should render results page with results and prev/next when there are more than 10 results', () => {
-    returnData = {
-      results: [
-        [
-          { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
-          { html: 'A1234BC', classes: '' },
-          { html: '2 April 1975', classes: '' },
-        ],
-        [
-          { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
-          { html: 'A1234BC', classes: '' },
-          { html: '2 April 1975', classes: '' },
-        ],
-        [
-          { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
-          { html: 'A1234BC', classes: '' },
-          { html: '2 April 1975', classes: '' },
-        ],
-        [
-          { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
-          { html: 'A1234BC', classes: '' },
-          { html: '2 April 1975', classes: '' },
-        ],
-        [
-          { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
-          { html: 'A1234BC', classes: '' },
-          { html: '2 April 1975', classes: '' },
-        ],
-        [
-          { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
-          { html: 'A1234BC', classes: '' },
-          { html: '2 April 1975', classes: '' },
-        ],
-        [
-          { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
-          { html: 'A1234BC', classes: '' },
-          { html: '2 April 1975', classes: '' },
-        ],
-        [
-          { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
-          { html: 'A1234BC', classes: '' },
-          { html: '2 April 1975', classes: '' },
-        ],
-        [
-          { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
-          { html: 'A1234BC', classes: '' },
-          { html: '2 April 1975', classes: '' },
-        ],
-        [
-          { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
-          { html: 'A1234BC', classes: '' },
-          { html: '2 April 1975', classes: '' },
-        ],
-        [
-          { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
-          { html: 'A1234BC', classes: '' },
-          { html: '2 April 1975', classes: '' },
-        ],
-      ],
-      numberOfPages: 12,
-      numberOfResults: 11,
-      next: 2,
-      previous: 1,
-    }
+  describe('GET /search/prisoner/results?search=A1234BC', () => {
+    it('should render prisoner results page with no results', () => {
+      returnData = {
+        results: [],
+        numberOfResults: 0,
+        numberOfPages: 0,
+        next: 0,
+        previous: 0,
+      }
 
-    return request(app)
-      .get('/search/results?search=A1234BC')
-      .expect('Content-Type', /html/)
-      .expect(res => {
-        expect(res.text).toContain('Search for a prisoner')
-        expect(res.text).toContain('id="search-results-true"')
-        expect(res.text).toContain('<p class="moj-pagination__results">')
-      })
+      return request(app)
+        .get('/search/prisoner/results?search=A1234BC')
+        .expect('Content-Type', /html/)
+        .expect(res => {
+          expect(res.text).toContain('Search for a prisoner')
+          expect(res.text).toContain('id="search-results-none"')
+        })
+    })
+  })
+
+  describe('GET /search/prisoner/results?search=A1234BC', () => {
+    it('should render prisoner results page with results and no next/prev when there are less than 11 results', () => {
+      returnData = {
+        results: [
+          [
+            { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
+            { html: 'A1234BC', classes: '' },
+            { html: '2 April 1975', classes: '' },
+          ],
+        ],
+        numberOfPages: 1,
+        numberOfResults: 1,
+        next: 1,
+        previous: 1,
+      }
+
+      return request(app)
+        .get('/search/prisoner/results?search=A1234BC')
+        .expect('Content-Type', /html/)
+        .expect(res => {
+          expect(res.text).toContain('Search for a prisoner')
+          expect(res.text).toContain('id="search-results-true"')
+          expect(res.text).not.toContain('<p class="moj-pagination__results">')
+        })
+    })
+
+    it('should render prisoner results page with results and prev/next when there are more than 10 results', () => {
+      returnData = {
+        results: [
+          [
+            { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
+            { html: 'A1234BC', classes: '' },
+            { html: '2 April 1975', classes: '' },
+          ],
+          [
+            { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
+            { html: 'A1234BC', classes: '' },
+            { html: '2 April 1975', classes: '' },
+          ],
+          [
+            { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
+            { html: 'A1234BC', classes: '' },
+            { html: '2 April 1975', classes: '' },
+          ],
+          [
+            { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
+            { html: 'A1234BC', classes: '' },
+            { html: '2 April 1975', classes: '' },
+          ],
+          [
+            { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
+            { html: 'A1234BC', classes: '' },
+            { html: '2 April 1975', classes: '' },
+          ],
+          [
+            { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
+            { html: 'A1234BC', classes: '' },
+            { html: '2 April 1975', classes: '' },
+          ],
+          [
+            { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
+            { html: 'A1234BC', classes: '' },
+            { html: '2 April 1975', classes: '' },
+          ],
+          [
+            { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
+            { html: 'A1234BC', classes: '' },
+            { html: '2 April 1975', classes: '' },
+          ],
+          [
+            { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
+            { html: 'A1234BC', classes: '' },
+            { html: '2 April 1975', classes: '' },
+          ],
+          [
+            { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
+            { html: 'A1234BC', classes: '' },
+            { html: '2 April 1975', classes: '' },
+          ],
+          [
+            { html: '<a href="/prisoner/A1234BC">Smith, John</a>', classes: '' },
+            { html: 'A1234BC', classes: '' },
+            { html: '2 April 1975', classes: '' },
+          ],
+        ],
+        numberOfPages: 12,
+        numberOfResults: 11,
+        next: 2,
+        previous: 1,
+      }
+
+      return request(app)
+        .get('/search/prisoner/results?search=A1234BC')
+        .expect('Content-Type', /html/)
+        .expect(res => {
+          expect(res.text).toContain('Search for a prisoner')
+          expect(res.text).toContain('id="search-results-true"')
+          expect(res.text).toContain('<p class="moj-pagination__results">')
+        })
+    })
   })
 })
--- server/views/pages/index.test.ts
+++ server/views/pages/index.test.ts
@@ -22,7 +22,7 @@ describe('Views - Home', () => {
     const $ = cheerio.load(compiledTemplate.render(viewContext))
 
     expect($('[data-test="book-visit"] .card__link').text()).toEqual('Book a visit')
-    expect($('[data-test="book-visit"] .card__link').attr('href')).toEqual('/search')
+    expect($('[data-test="book-visit"] .card__link').attr('href')).toEqual('/search/prisoner')
 
     expect($('[data-test="cancel-visit"] .card__link').text()).toEqual('Cancel a visit')
     expect($('[data-test="cancel-visit"] .card__link').attr('href')).toEqual('/cancel')
