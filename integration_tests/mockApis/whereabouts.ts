2b16e178dbfe7dae64899cf68dfe2dc06b6272a7
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-04-25 11:25:52 +0100

    VB-570: Add mocks for tests

---
+++ integration_tests/mockApis/whereabouts.ts
@@ -0,0 +1,32 @@
+import { SuperAgentRequest } from 'superagent'
+import { stubFor } from './wiremock'
+
+export default {
+  stubGetOffenderEvents: (): SuperAgentRequest => {
+    const results = {
+      bookingId: 123456,
+      eventClass: 'ABC',
+      eventDate: '2022-04-25T09:35:34.489Z',
+      eventSource: 'ABC',
+      eventSourceDesc: 'Source Desc',
+      eventStatus: 'ABC',
+      eventSubType: 'ABCDEF',
+      eventSubTypeDesc: 'ABCDEF Desc',
+      eventType: 'ABC',
+      eventTypeDesc: 'ABC Desc',
+      startTime: '2022-04-25T09:35:34.489Z',
+    }
+
+    return stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/events/*?fromDate=*&toDate=*',
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: results,
+      },
+    })
+  },
+}
--- integration_tests/plugins/index.ts
+++ integration_tests/plugins/index.ts
@@ -2,6 +2,8 @@ import { resetStubs } from '../mockApis/wiremock'
 
 import auth from '../mockApis/auth'
 import tokenVerification from '../mockApis/tokenVerification'
+import prisonerContactRegistry from '../mockApis/prisonerContactRegistry'
+import whereaboutsOffenderEvents from '../mockApis/whereabouts'
 
 export default (on: (string, Record) => void): void => {
   on('task', {
@@ -14,5 +16,9 @@ export default (on: (string, Record) => void): void => {
     stubAuthPing: auth.stubPing,
 
     stubTokenVerificationPing: tokenVerification.stubPing,
+
+    stubGetPrisonerContacts: prisonerContactRegistry.stubGetPrisonerContacts,
+
+    stubGetOffenderEvents: whereaboutsOffenderEvents.stubGetOffenderEvents,
   })
 }
