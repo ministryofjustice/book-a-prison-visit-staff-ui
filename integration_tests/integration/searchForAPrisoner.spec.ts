f43da29fb79c1eec315a14d89ff51f6a806a2f1d
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2022-05-04 15:35:47 +0100

    VB-570: Change grab href and go to click link (#157)

--- integration_tests/integration/searchForAPrisoner.spec.ts
+++ integration_tests/integration/searchForAPrisoner.spec.ts
@@ -54,19 +54,13 @@ context('Search for a prisoner', () => {
       cy.task('stubGetPrisoners', results)
       cy.signIn()
       const indexPage = Page.verifyOnPage(IndexPage)
-      indexPage
-        .bookAVisitLink()
-        .invoke('attr', 'href')
-        .then(href => {
-          cy.visit(href)
+      indexPage.bookAVisitLink().click()
+      const searchForAPrisonerPage = Page.verifyOnPage(SearchForAPrisonerPage)
+      searchForAPrisonerPage.searchInput().clear().type(prisonerNumber)
+      searchForAPrisonerPage.searchButton().click()
 
-          const searchForAPrisonerPage = Page.verifyOnPage(SearchForAPrisonerPage)
-          searchForAPrisonerPage.searchInput().clear().type(prisonerNumber)
-          searchForAPrisonerPage.searchButton().click()
-
-          const searchForAPrisonerResultsPage = Page.verifyOnPage(SearchForAPrisonerResultsPage)
-          searchForAPrisonerResultsPage.noResults()
-        })
+      const searchForAPrisonerResultsPage = Page.verifyOnPage(SearchForAPrisonerResultsPage)
+      searchForAPrisonerResultsPage.noResults()
     })
   })
 
@@ -77,34 +71,29 @@ context('Search for a prisoner', () => {
       cy.task('stubGetPrisoners', results)
       cy.signIn()
       const indexPage = Page.verifyOnPage(IndexPage)
-      indexPage
-        .bookAVisitLink()
-        .invoke('attr', 'href')
-        .then(href => {
-          cy.visit(href)
+      indexPage.bookAVisitLink().click()
 
-          const searchForAPrisonerPage = Page.verifyOnPage(SearchForAPrisonerPage)
-          searchForAPrisonerPage.searchInput().clear().type(prisonerNumber)
-          searchForAPrisonerPage.searchButton().click()
+      const searchForAPrisonerPage = Page.verifyOnPage(SearchForAPrisonerPage)
+      searchForAPrisonerPage.searchInput().clear().type(prisonerNumber)
+      searchForAPrisonerPage.searchButton().click()
 
-          const searchForAPrisonerResultsPage = Page.verifyOnPage(SearchForAPrisonerResultsPage)
-          searchForAPrisonerResultsPage.hasResults()
-          searchForAPrisonerResultsPage.pagingLinks().should('not.exist')
-          searchForAPrisonerResultsPage.resultRows().should('have.length', results.content.length)
+      const searchForAPrisonerResultsPage = Page.verifyOnPage(SearchForAPrisonerResultsPage)
+      searchForAPrisonerResultsPage.hasResults()
+      searchForAPrisonerResultsPage.pagingLinks().should('not.exist')
+      searchForAPrisonerResultsPage.resultRows().should('have.length', results.content.length)
 
-          results.content.forEach((prisoner, index) => {
-            searchForAPrisonerResultsPage
-              .resultRows()
-              .eq(index)
-              .within(() => {
-                cy.get('td').eq(0).contains(`${prisoner.lastName}, ${prisoner.firstName}`)
-                cy.get('td').eq(1).contains(prisoner.prisonerNumber)
-                cy.get('td')
-                  .eq(2)
-                  .contains(prisonerDatePretty({ dateToFormat: prisoner.dateOfBirth }))
-              })
+      results.content.forEach((prisoner, index) => {
+        searchForAPrisonerResultsPage
+          .resultRows()
+          .eq(index)
+          .within(() => {
+            cy.get('td').eq(0).contains(`${prisoner.lastName}, ${prisoner.firstName}`)
+            cy.get('td').eq(1).contains(prisoner.prisonerNumber)
+            cy.get('td')
+              .eq(2)
+              .contains(prisonerDatePretty({ dateToFormat: prisoner.dateOfBirth }))
           })
-        })
+      })
     })
   })
 
@@ -118,50 +107,45 @@ context('Search for a prisoner', () => {
       cy.task('stubGetPrisoners', resultsPage1)
       cy.signIn()
       const indexPage = Page.verifyOnPage(IndexPage)
-      indexPage
-        .bookAVisitLink()
-        .invoke('attr', 'href')
-        .then(href => {
-          cy.visit(href)
+      indexPage.bookAVisitLink().click()
 
-          const searchForAPrisonerPage = Page.verifyOnPage(SearchForAPrisonerPage)
-          searchForAPrisonerPage.searchInput().clear().type(prisonerNumber)
-          searchForAPrisonerPage.searchButton().click()
+      const searchForAPrisonerPage = Page.verifyOnPage(SearchForAPrisonerPage)
+      searchForAPrisonerPage.searchInput().clear().type(prisonerNumber)
+      searchForAPrisonerPage.searchButton().click()
 
-          const searchForAPrisonerResultsPage = Page.verifyOnPage(SearchForAPrisonerResultsPage)
-          searchForAPrisonerResultsPage.hasResults()
-          searchForAPrisonerResultsPage.pagingLinks().should('exist')
-          searchForAPrisonerResultsPage.resultRows().should('have.length', pageSize)
+      const searchForAPrisonerResultsPage = Page.verifyOnPage(SearchForAPrisonerResultsPage)
+      searchForAPrisonerResultsPage.hasResults()
+      searchForAPrisonerResultsPage.pagingLinks().should('exist')
+      searchForAPrisonerResultsPage.resultRows().should('have.length', pageSize)
 
-          resultsPage1.content.forEach((prisoner, index) => {
-            searchForAPrisonerResultsPage
-              .resultRows()
-              .eq(index)
-              .within(() => {
-                cy.get('td').eq(0).contains(`${prisoner.lastName}, ${prisoner.firstName}`)
-                cy.get('td').eq(1).contains(prisoner.prisonerNumber)
-                cy.get('td')
-                  .eq(2)
-                  .contains(prisonerDatePretty({ dateToFormat: prisoner.dateOfBirth }))
-              })
+      resultsPage1.content.forEach((prisoner, index) => {
+        searchForAPrisonerResultsPage
+          .resultRows()
+          .eq(index)
+          .within(() => {
+            cy.get('td').eq(0).contains(`${prisoner.lastName}, ${prisoner.firstName}`)
+            cy.get('td').eq(1).contains(prisoner.prisonerNumber)
+            cy.get('td')
+              .eq(2)
+              .contains(prisonerDatePretty({ dateToFormat: prisoner.dateOfBirth }))
           })
+      })
 
-          cy.task('stubGetPrisoners', resultsPage2)
-          searchForAPrisonerResultsPage.nextPageLink().click()
-          searchForAPrisonerResultsPage.hasResults()
-          searchForAPrisonerResultsPage.pagingLinks().should('exist')
-          searchForAPrisonerResultsPage.resultRows().should('have.length', 1)
+      cy.task('stubGetPrisoners', resultsPage2)
+      searchForAPrisonerResultsPage.nextPageLink().click()
+      searchForAPrisonerResultsPage.hasResults()
+      searchForAPrisonerResultsPage.pagingLinks().should('exist')
+      searchForAPrisonerResultsPage.resultRows().should('have.length', 1)
 
-          searchForAPrisonerResultsPage
-            .resultRows()
-            .eq(0)
-            .within(() => {
-              cy.get('td').eq(0).contains(`${resultsPage2.content[0].lastName}, ${resultsPage2.content[0].firstName}`)
-              cy.get('td').eq(1).contains(resultsPage2.content[0].prisonerNumber)
-              cy.get('td')
-                .eq(2)
-                .contains(prisonerDatePretty({ dateToFormat: resultsPage2.content[0].dateOfBirth }))
-            })
+      searchForAPrisonerResultsPage
+        .resultRows()
+        .eq(0)
+        .within(() => {
+          cy.get('td').eq(0).contains(`${resultsPage2.content[0].lastName}, ${resultsPage2.content[0].firstName}`)
+          cy.get('td').eq(1).contains(resultsPage2.content[0].prisonerNumber)
+          cy.get('td')
+            .eq(2)
+            .contains(prisonerDatePretty({ dateToFormat: resultsPage2.content[0].dateOfBirth }))
         })
     })
   })
@@ -189,120 +173,113 @@ context('Search for a prisoner', () => {
       cy.task('stubGetPrisoners', results)
       cy.signIn()
       const indexPage = Page.verifyOnPage(IndexPage)
-      indexPage
-        .bookAVisitLink()
-        .invoke('attr', 'href')
-        .then(href => {
-          cy.visit(href)
+      indexPage.bookAVisitLink().click()
 
-          const searchForAPrisonerPage = Page.verifyOnPage(SearchForAPrisonerPage)
-          searchForAPrisonerPage.searchInput().clear().type(prisonerNumber)
-          searchForAPrisonerPage.searchButton().click()
+      const searchForAPrisonerPage = Page.verifyOnPage(SearchForAPrisonerPage)
+      searchForAPrisonerPage.searchInput().clear().type(prisonerNumber)
+      searchForAPrisonerPage.searchButton().click()
 
-          const searchForAPrisonerResultsPage = Page.verifyOnPage(SearchForAPrisonerResultsPage)
-          searchForAPrisonerResultsPage.resultRows().should('have.length', results.content.length)
-          searchForAPrisonerResultsPage
-            .firstResultLink()
-            .invoke('text')
-            .then(text => {
-              cy.task('stubGetBookings', prisonerNumber)
-              cy.task('stubGetOffender', offender)
-              cy.task('stubGetVisitBalances', { offenderNo: prisonerNumber, visitBalances })
-              cy.task('stubGetUpcomingVisits', { offenderNo: prisonerNumber, upcomingVisits })
-              cy.task('stubGetPastVisits', { offenderNo: prisonerNumber, pastVisits })
-              cy.task('stubGetPrisonerSocialContacts', prisonerNumber)
-              searchForAPrisonerResultsPage.firstResultLink().click()
+      const searchForAPrisonerResultsPage = Page.verifyOnPage(SearchForAPrisonerResultsPage)
+      searchForAPrisonerResultsPage.resultRows().should('have.length', results.content.length)
+      searchForAPrisonerResultsPage
+        .firstResultLink()
+        .invoke('text')
+        .then(pageTitle => {
+          cy.task('stubGetBookings', prisonerNumber)
+          cy.task('stubGetOffender', offender)
+          cy.task('stubGetVisitBalances', { offenderNo: prisonerNumber, visitBalances })
+          cy.task('stubGetUpcomingVisits', { offenderNo: prisonerNumber, upcomingVisits })
+          cy.task('stubGetPastVisits', { offenderNo: prisonerNumber, pastVisits })
+          cy.task('stubGetPrisonerSocialContacts', prisonerNumber)
+          searchForAPrisonerResultsPage.firstResultLink().click()
 
-              const prisonerProfilePage = new PrisonerProfilePage(text)
-              const isIntTest = true
+          const prisonerProfilePage = new PrisonerProfilePage(pageTitle)
+          const isIntTest = true
 
-              prisonerProfilePage.prisonNumber().contains(prisonerNumber)
-              prisonerProfilePage.dateOfBirth().contains(prisonerDatePretty({ dateToFormat: offender.dateOfBirth }))
-              prisonerProfilePage
-                .location()
-                .contains(`${offender.assignedLivingUnit.description}, ${offender.assignedLivingUnit.agencyName}`)
-              prisonerProfilePage.category().contains(offender.category)
-              prisonerProfilePage.incentiveLevel().contains(offender.privilegeSummary.iepLevel)
-              prisonerProfilePage.convictionStatus().contains('Convicted')
-              prisonerProfilePage.alertCount().contains(offender.activeAlertCount)
-              prisonerProfilePage.remainingVOs().contains(visitBalances.remainingVo)
-              prisonerProfilePage.remainingPVOs().contains(visitBalances.remainingPvo)
-              prisonerProfilePage.flaggedAlerts().eq(0).contains(offender.alerts[0].alertCodeDescription)
+          prisonerProfilePage.prisonNumber().contains(prisonerNumber)
+          prisonerProfilePage.dateOfBirth().contains(prisonerDatePretty({ dateToFormat: offender.dateOfBirth }))
+          prisonerProfilePage
+            .location()
+            .contains(`${offender.assignedLivingUnit.description}, ${offender.assignedLivingUnit.agencyName}`)
+          prisonerProfilePage.category().contains(offender.category)
+          prisonerProfilePage.incentiveLevel().contains(offender.privilegeSummary.iepLevel)
+          prisonerProfilePage.convictionStatus().contains('Convicted')
+          prisonerProfilePage.alertCount().contains(offender.activeAlertCount)
+          prisonerProfilePage.remainingVOs().contains(visitBalances.remainingVo)
+          prisonerProfilePage.remainingPVOs().contains(visitBalances.remainingPvo)
+          prisonerProfilePage.flaggedAlerts().eq(0).contains(offender.alerts[0].alertCodeDescription)
 
-              prisonerProfilePage.visitTabVORemaining().contains(visitBalances.remainingVo)
-              prisonerProfilePage
-                .visitTabVOLastAdjustment()
-                .contains(prisonerDatePretty({ dateToFormat: visitBalances.latestIepAdjustDate }))
-              prisonerProfilePage
-                .visitTabVONextAdjustment()
-                .contains(nextIepAdjustDate(visitBalances.latestIepAdjustDate))
-              prisonerProfilePage.visitTabPVORemaining().contains(visitBalances.remainingPvo)
-              prisonerProfilePage
-                .visitTabPVOLastAdjustment()
-                .contains(prisonerDatePretty({ dateToFormat: visitBalances.latestPrivIepAdjustDate }))
-              prisonerProfilePage
-                .visitTabPVONextAdjustment()
-                .contains(nextPrivIepAdjustDate(visitBalances.latestPrivIepAdjustDate))
+          prisonerProfilePage.visitTabVORemaining().contains(visitBalances.remainingVo)
+          prisonerProfilePage
+            .visitTabVOLastAdjustment()
+            .contains(prisonerDatePretty({ dateToFormat: visitBalances.latestIepAdjustDate }))
+          prisonerProfilePage.visitTabVONextAdjustment().contains(nextIepAdjustDate(visitBalances.latestIepAdjustDate))
+          prisonerProfilePage.visitTabPVORemaining().contains(visitBalances.remainingPvo)
+          prisonerProfilePage
+            .visitTabPVOLastAdjustment()
+            .contains(prisonerDatePretty({ dateToFormat: visitBalances.latestPrivIepAdjustDate }))
+          prisonerProfilePage
+            .visitTabPVONextAdjustment()
+            .contains(nextPrivIepAdjustDate(visitBalances.latestPrivIepAdjustDate))
 
-              prisonerProfilePage.selectActiveAlertsTab()
-              prisonerProfilePage.alertsTabType().eq(0).contains(offender.alerts[0].alertTypeDescription)
-              prisonerProfilePage.alertsTabCode().eq(0).contains(offender.alerts[0].alertCodeDescription)
-              prisonerProfilePage.alertsTabComment().eq(0).contains(offender.alerts[0].comment)
-              prisonerProfilePage
-                .alertsTabCreated()
-                .eq(0)
-                .contains(prisonerDatePretty({ dateToFormat: offender.alerts[0].dateCreated }))
-              prisonerProfilePage.alertsTabExpires().eq(0).contains('Not entered')
+          prisonerProfilePage.selectActiveAlertsTab()
+          prisonerProfilePage.alertsTabType().eq(0).contains(offender.alerts[0].alertTypeDescription)
+          prisonerProfilePage.alertsTabCode().eq(0).contains(offender.alerts[0].alertCodeDescription)
+          prisonerProfilePage.alertsTabComment().eq(0).contains(offender.alerts[0].comment)
+          prisonerProfilePage
+            .alertsTabCreated()
+            .eq(0)
+            .contains(prisonerDatePretty({ dateToFormat: offender.alerts[0].dateCreated }))
+          prisonerProfilePage.alertsTabExpires().eq(0).contains('Not entered')
 
-              prisonerProfilePage.selectUpcomingVisitsTab()
-              prisonerProfilePage
-                .upcomingTabType()
-                .eq(0)
-                .contains(
-                  formatVisitType({
-                    visitType: upcomingVisits[0].visitType,
-                    visitRestriction: upcomingVisits[0].visitRestriction,
-                    isIntTest,
-                  })
-                )
-              prisonerProfilePage.upcomingTabLocation().eq(0).contains('Hewell (HMP)')
-              prisonerProfilePage
-                .upcomingTabDateAndTime()
-                .eq(0)
-                .contains(
-                  visitDateAndTime({
-                    startTimestamp: upcomingVisits[0].startTimestamp,
-                    endTimestamp: upcomingVisits[0].endTimestamp,
-                    isIntTest,
-                  })
-                )
-              prisonerProfilePage.upcomingTabVisitors().eq(0).contains('John Smith')
+          prisonerProfilePage.selectUpcomingVisitsTab()
+          prisonerProfilePage
+            .upcomingTabType()
+            .eq(0)
+            .contains(
+              formatVisitType({
+                visitType: upcomingVisits[0].visitType,
+                visitRestriction: upcomingVisits[0].visitRestriction,
+                isIntTest,
+              })
+            )
+          prisonerProfilePage.upcomingTabLocation().eq(0).contains('Hewell (HMP)')
+          prisonerProfilePage
+            .upcomingTabDateAndTime()
+            .eq(0)
+            .contains(
+              visitDateAndTime({
+                startTimestamp: upcomingVisits[0].startTimestamp,
+                endTimestamp: upcomingVisits[0].endTimestamp,
+                isIntTest,
+              })
+            )
+          prisonerProfilePage.upcomingTabVisitors().eq(0).contains('John Smith')
 
-              prisonerProfilePage.selectVisitsHistoryTab()
-              prisonerProfilePage
-                .pastTabType()
-                .eq(0)
-                .contains(
-                  formatVisitType({
-                    visitType: pastVisits[0].visitType,
-                    visitRestriction: pastVisits[0].visitRestriction,
-                    isIntTest,
-                  })
-                )
-              prisonerProfilePage.pastTabLocation().eq(0).contains('Hewell (HMP)')
-              prisonerProfilePage
-                .pastTabDateAndTime()
-                .eq(0)
-                .contains(
-                  visitDateAndTime({
-                    startTimestamp: pastVisits[0].startTimestamp,
-                    endTimestamp: pastVisits[0].endTimestamp,
-                    isIntTest,
-                  })
-                )
-              prisonerProfilePage.pastTabVisitors().eq(0).contains('John Smith')
-              prisonerProfilePage.pastTabStatus().eq(0).contains(properCase(pastVisits[0].visitStatus))
-            })
+          prisonerProfilePage.selectVisitsHistoryTab()
+          prisonerProfilePage
+            .pastTabType()
+            .eq(0)
+            .contains(
+              formatVisitType({
+                visitType: pastVisits[0].visitType,
+                visitRestriction: pastVisits[0].visitRestriction,
+                isIntTest,
+              })
+            )
+          prisonerProfilePage.pastTabLocation().eq(0).contains('Hewell (HMP)')
+          prisonerProfilePage
+            .pastTabDateAndTime()
+            .eq(0)
+            .contains(
+              visitDateAndTime({
+                startTimestamp: pastVisits[0].startTimestamp,
+                endTimestamp: pastVisits[0].endTimestamp,
+                isIntTest,
+              })
+            )
+          prisonerProfilePage.pastTabVisitors().eq(0).contains('John Smith')
+          prisonerProfilePage.pastTabStatus().eq(0).contains(properCase(pastVisits[0].visitStatus))
         })
     })
   })
