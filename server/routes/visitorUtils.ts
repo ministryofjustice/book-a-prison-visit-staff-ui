8ce8e2400fc9f14a39263db11e41de319814fa32
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2022-05-13 07:39:13 +0100

    Remove check session function as now in middleware (#174)
    * Remove unused function checkSession
    
    * Remove unused function checkSession

--- server/routes/visitorUtils.ts
+++ server/routes/visitorUtils.ts
@@ -1,7 +1,6 @@
-import type { Request, Response } from 'express'
-import { VisitSessionData, VisitSlot, VisitSlotList } from '../@types/bapv'
+import type { Request } from 'express'
+import { VisitSlot, VisitSlotList } from '../@types/bapv'
 import { SupportType, VisitorSupport } from '../data/visitSchedulerApiTypes'
-import { isValidPrisonerNumber } from './validationChecks'
 
 export const getSelectedSlot = (slotsList: VisitSlotList, selectedSlot: string): VisitSlot => {
   return Object.values(slotsList)
@@ -10,55 +9,6 @@ export const getSelectedSlot = (slotsList: VisitSlotList, selectedSlot: string):
       return allSlots.concat(slot.slots.morning, slot.slots.afternoon)
     }, [])
     .find(slot => slot.id === selectedSlot)
-}
-
-export const checkSession = ({
-  stage,
-  visitSessionData,
-  res,
-}: {
-  stage: number
-  visitSessionData: VisitSessionData
-  res: Response
-  // eslint-disable-next-line consistent-return
-}): void => {
-  if (!visitSessionData) {
-    return res.redirect('/search/?error=missing-session')
-  }
-
-  if (
-    !visitSessionData.prisoner ||
-    !visitSessionData.prisoner.name ||
-    !isValidPrisonerNumber(visitSessionData.prisoner.offenderNo || '') ||
-    !visitSessionData.prisoner.dateOfBirth ||
-    !visitSessionData.prisoner.location
-  ) {
-    return res.redirect('/search/?error=missing-prisoner')
-  }
-
-  if (stage > 1 && (!visitSessionData.visitors || visitSessionData.visitors.length === 0)) {
-    return res.redirect(`/prisoner/${visitSessionData.prisoner.offenderNo}?error=missing-visitors`)
-  }
-
-  if (
-    stage > 2 &&
-    (!visitSessionData.visit ||
-      !visitSessionData.visit.id ||
-      !visitSessionData.visit.availableTables ||
-      !visitSessionData.visit.startTimestamp ||
-      !visitSessionData.visit.endTimestamp)
-  ) {
-    return res.redirect(`/prisoner/${visitSessionData.prisoner.offenderNo}?error=missing-visit`)
-  }
-
-  if (
-    stage > 4 &&
-    (!visitSessionData.mainContact ||
-      !visitSessionData.mainContact.phoneNumber ||
-      (!visitSessionData.mainContact.contact && !visitSessionData.mainContact.contactName))
-  ) {
-    return res.redirect(`/prisoner/${visitSessionData.prisoner.offenderNo}?error=missing-main-contact`)
-  }
 }
 
 export const getFlashFormValues = (req: Request): Record<string, string | string[]> => {
