a7819d63c6d1533c5395c8b24188cee6bc8e7513
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2022-06-09 08:42:39 +0100

    VB-735: Select date to view view visits (#209)
    * VB-735: Add date picker for view visits
    
    * Add form and styling etc
    
    * More CSS tweaks
    
    * Remove console.log
    
    * Add tests
    
    * Use sass function

---
+++ assets/js/viewVisitsDatePicker.js
@@ -0,0 +1,18 @@
+const viewAnotherDateToggle = document.querySelector('.bapv-another-date__toggle button')
+
+if (viewAnotherDateToggle) {
+  function toggleDatePicker(event) {
+    event.preventDefault()
+    const datePicker = document.querySelector('.bapv-another-date__form')
+
+    if (datePicker.classList.contains('moj-js-hidden')) {
+      viewAnotherDateToggle.setAttribute('aria-expanded', 'false')
+      datePicker.classList.remove('moj-js-hidden')
+    } else {
+      viewAnotherDateToggle.setAttribute('aria-expanded', 'true')
+      datePicker.classList.add('moj-js-hidden')
+    }
+  }
+
+  viewAnotherDateToggle.addEventListener('click', toggleDatePicker)
+}
