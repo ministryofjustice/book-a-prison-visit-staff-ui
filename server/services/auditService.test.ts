903858e292ac50697988ed65c24a0f4c0b9499c0
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-06-28 15:46:52 +0100

    Add tests for all audit messages

--- server/services/auditService.test.ts
+++ server/services/auditService.test.ts
@@ -21,11 +21,59 @@ describe('Audit service', () => {
     })
 
     afterEach(() => {
-      jest.resetAllMocks()
+      jest.clearAllMocks()
     })
 
-    it('Retrieves and processes prisoner and approved visitor details', async () => {
+    it('sends a prisoner search audit message', async () => {
       await auditService.prisonerSearch('sdsd', 'HEI', 'username')
+
+      expect(sqsClientInstance.send).toHaveBeenCalledTimes(1)
+    })
+
+    it('sends a view prisoner audit message', async () => {
+      await auditService.viewPrisoner('A1234BC', 'HEI', 'username')
+
+      expect(sqsClientInstance.send).toHaveBeenCalledTimes(1)
+    })
+
+    it('sends a visit reserved audit message', async () => {
+      await auditService.reservedVisit('ref1234', 'A1234BC', 'HEI', 'username')
+
+      expect(sqsClientInstance.send).toHaveBeenCalledTimes(1)
+    })
+
+    it('sends a visit booked audit message', async () => {
+      await auditService.bookedVisit('ref1234', 'A1234BC', 'HEI', ['abc123', 'bcd321'], 'username')
+
+      expect(sqsClientInstance.send).toHaveBeenCalledTimes(1)
+    })
+
+    it('sends a cancelled visit audit message', async () => {
+      await auditService.cancelledVisit('ref1234', 'A1234BC', 'HEI', 'reason for cancellation', 'username')
+
+      expect(sqsClientInstance.send).toHaveBeenCalledTimes(1)
+    })
+
+    it('sends a viewd visits page audit message', async () => {
+      await auditService.viewedVisits('2022-06-01T12:12:12', 'HEI', 'username')
+
+      expect(sqsClientInstance.send).toHaveBeenCalledTimes(1)
+    })
+
+    it('sends a visit list printed audit message', async () => {
+      await auditService.printedVisitList('2022-06-01T12:12:12', 'HEI', 'username')
+
+      expect(sqsClientInstance.send).toHaveBeenCalledTimes(1)
+    })
+
+    it('sends a page view audit message', async () => {
+      await auditService.pageView('Find a slot', 'username')
+
+      expect(sqsClientInstance.send).toHaveBeenCalledTimes(1)
+    })
+
+    it('sends a zero VO overridden audit message', async () => {
+      await auditService.overrodeZeroVO('ref1234', 'A1234BC', 'reason for cancellation', 'username')
 
       expect(sqsClientInstance.send).toHaveBeenCalledTimes(1)
     })
