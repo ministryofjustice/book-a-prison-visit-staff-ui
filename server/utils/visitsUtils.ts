70101c42c9df6fe261fc4269b983efd5654606f8
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-07-29 06:48:30 +0100

    Refactor getVisitSlotsFromBookedVisits

--- server/utils/visitsUtils.ts
+++ server/utils/visitsUtils.ts
@@ -14,30 +14,30 @@ export const getVisitSlotsFromBookedVisits = (
   const slots: {
     OPEN: VisitsPageSlot[]
     CLOSED: VisitsPageSlot[]
+    UNKNOWN: VisitsPageSlot[]
   } = {
     OPEN: [],
     CLOSED: [],
+    UNKNOWN: [],
   }
 
   visits.forEach((visit: ExtendedVisitInformation) => {
-    const visitRestriction = visit.visitRestriction === 'OPEN' ? 'OPEN' : 'CLOSED'
-
-    let matchingSlot = slots[visitRestriction].findIndex(slot => slot.visitTime === visit.visitTime)
+    let matchingSlot = slots[visit.visitRestriction].findIndex(slot => slot.visitTime === visit.visitTime)
 
     if (matchingSlot < 0) {
-      slots[visitRestriction].push({
+      slots[visit.visitRestriction].push({
         visitTime: visit.visitTime,
-        visitType: visitRestriction,
+        visitType: visit.visitRestriction,
         sortField: visit.startTimestamp,
         adults: 0,
         children: 0,
       })
 
-      matchingSlot = slots[visitRestriction].length - 1
+      matchingSlot = slots[visit.visitRestriction].length - 1
     }
 
-    slots[visitRestriction][matchingSlot].adults += visit.visitors.filter(visitor => visitor.adult).length
-    slots[visitRestriction][matchingSlot].children += visit.visitors.filter(visitor => !visitor.adult).length
+    slots[visit.visitRestriction][matchingSlot].adults += visit.visitors.filter(visitor => visitor.adult).length
+    slots[visit.visitRestriction][matchingSlot].children += visit.visitors.filter(visitor => !visitor.adult).length
   })
 
   let firstSlotTime: string
@@ -46,11 +46,14 @@ export const getVisitSlotsFromBookedVisits = (
     firstSlotTime = slots.OPEN.sort(sortByTimestamp)[0].visitTime
   } else if (slots.CLOSED.length > 0) {
     firstSlotTime = slots.CLOSED.sort(sortByTimestamp)[0].visitTime
+  } else if (slots.UNKNOWN.length > 0) {
+    firstSlotTime = slots.UNKNOWN.sort(sortByTimestamp)[0].visitTime
   }
 
   return {
     openSlots: slots.OPEN,
     closedSlots: slots.CLOSED,
+    unknownSlots: slots.UNKNOWN,
     firstSlotTime,
   }
 }
