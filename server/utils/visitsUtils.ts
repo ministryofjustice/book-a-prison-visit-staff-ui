10815c9ffd793a51175b45e02748f4dc688a2ce2
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-07-29 06:48:30 +0100

    Refactor getVisitSlotsFromBookedVisits

--- server/utils/visitsUtils.ts
+++ server/utils/visitsUtils.ts
@@ -9,79 +9,49 @@ const getVisitSlotsFromBookedVisits = (
   unknownSlots: VisitsPageSlot[]
   firstSlotTime: string
 } => {
-  const openSlots: VisitsPageSlot[] = []
-  const closedSlots: VisitsPageSlot[] = []
-  const unknownSlots: VisitsPageSlot[] = []
+  const slots: {
+    OPEN: VisitsPageSlot[]
+    CLOSED: VisitsPageSlot[]
+    UNKNOWN: VisitsPageSlot[]
+  } = {
+    OPEN: [],
+    CLOSED: [],
+    UNKNOWN: [],
+  }
 
   visits.forEach((visit: ExtendedVisitInformation) => {
-    if (visit.visitRestriction === 'OPEN') {
-      let matchingSlot = openSlots.findIndex(slot => slot.visitTime === visit.visitTime)
+    let matchingSlot = slots[visit.visitRestriction].findIndex(slot => slot.visitTime === visit.visitTime)
 
-      if (matchingSlot < 0) {
-        openSlots.push({
-          visitTime: visit.visitTime,
-          visitType: 'OPEN',
-          sortField: visit.startTimestamp,
-          adults: 0,
-          children: 0,
-        })
+    if (matchingSlot < 0) {
+      slots[visit.visitRestriction].push({
+        visitTime: visit.visitTime,
+        visitType: visit.visitRestriction,
+        sortField: visit.startTimestamp,
+        adults: 0,
+        children: 0,
+      })
 
-        matchingSlot = openSlots.length - 1
-      }
-
-      openSlots[matchingSlot].adults += visit.visitors.filter(visitor => visitor.adult).length
-      openSlots[matchingSlot].children += visit.visitors.filter(visitor => !visitor.adult).length
-    } else if (visit.visitRestriction === 'CLOSED') {
-      let matchingSlot = closedSlots.findIndex(slot => slot.visitTime === visit.visitTime)
-
-      if (matchingSlot < 0) {
-        closedSlots.push({
-          visitTime: visit.visitTime,
-          visitType: 'CLOSED',
-          sortField: visit.startTimestamp,
-          adults: 0,
-          children: 0,
-        })
-
-        matchingSlot = closedSlots.length - 1
-      }
-
-      closedSlots[matchingSlot].adults += visit.visitors.filter(visitor => visitor.adult).length
-      closedSlots[matchingSlot].children += visit.visitors.filter(visitor => !visitor.adult).length
-    } else {
-      let matchingSlot = unknownSlots.findIndex(slot => slot.visitTime === visit.visitTime)
-
-      if (matchingSlot < 0) {
-        unknownSlots.push({
-          visitTime: visit.visitTime,
-          visitType: 'UNKNOWN',
-          sortField: visit.startTimestamp,
-          adults: 0,
-          children: 0,
-        })
-
-        matchingSlot = unknownSlots.length - 1
-      }
-
-      unknownSlots[matchingSlot].adults += visit.visitors.filter(visitor => visitor.adult).length
-      unknownSlots[matchingSlot].children += visit.visitors.filter(visitor => !visitor.adult).length
+      matchingSlot = slots[visit.visitRestriction].length - 1
     }
+
+    slots[visit.visitRestriction][matchingSlot].adults += visit.visitors.filter(visitor => visitor.adult).length
+    slots[visit.visitRestriction][matchingSlot].children += visit.visitors.filter(visitor => !visitor.adult).length
   })
 
   let firstSlotTime: string
 
-  if (openSlots.length > 0) {
-    firstSlotTime = openSlots.sort(sortByTimestamp)[0].visitTime
-  } else if (closedSlots.length > 0) {
-    firstSlotTime = closedSlots.sort(sortByTimestamp)[0].visitTime
-  } else if (unknownSlots.length > 0) {
-    firstSlotTime = unknownSlots.sort(sortByTimestamp)[0].visitTime
+  if (slots.OPEN.length > 0) {
+    firstSlotTime = slots.OPEN.sort(sortByTimestamp)[0].visitTime
+  } else if (slots.CLOSED.length > 0) {
+    firstSlotTime = slots.CLOSED.sort(sortByTimestamp)[0].visitTime
+  } else if (slots.UNKNOWN.length > 0) {
+    firstSlotTime = slots.UNKNOWN.sort(sortByTimestamp)[0].visitTime
   }
 
   return {
-    openSlots,
-    closedSlots,
-    unknownSlots,
+    openSlots: slots.OPEN,
+    closedSlots: slots.CLOSED,
+    unknownSlots: slots.UNKNOWN,
     firstSlotTime,
   }
 }
