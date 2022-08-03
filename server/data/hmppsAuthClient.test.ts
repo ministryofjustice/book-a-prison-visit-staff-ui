6cf1dbde1503d79d1d06a987c142f848e8517faf
hutcheonb-moj <106151419+hutcheonb-moj@users.noreply.github.com>
2022-08-03 09:39:19 +0100

    Routes level test normalisation (#269)
    * client-level, normalisation of tests
    
    * route level, test normalisation
    
    * routes level, test normalisation, second pass

--- server/data/hmppsAuthClient.test.ts
+++ server/data/hmppsAuthClient.test.ts
@@ -75,7 +75,7 @@ describe('hmppsAuthClient', () => {
       const output = await hmppsAuthClient.getSystemClientToken(username)
 
       expect(output).toEqual(token.access_token)
-      expect(tokenStore.setToken).toBeCalledWith('Bob', token.access_token, 240)
+      expect(tokenStore.setToken).toHaveBeenCalledWith('Bob', token.access_token, 240)
     })
 
     it('should return token from HMPPS Auth without username', async () => {
@@ -90,7 +90,7 @@ describe('hmppsAuthClient', () => {
       const output = await hmppsAuthClient.getSystemClientToken()
 
       expect(output).toEqual(token.access_token)
-      expect(tokenStore.setToken).toBeCalledWith('%ANONYMOUS%', token.access_token, 240)
+      expect(tokenStore.setToken).toHaveBeenCalledWith('%ANONYMOUS%', token.access_token, 240)
     })
   })
 })
