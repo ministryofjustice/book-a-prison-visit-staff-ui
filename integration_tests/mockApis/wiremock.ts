f8d9d704be51c6fe65302b80dcc81a6f5954cfb6
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2022-08-18 12:35:22 +0100

    Add token verification integration tests (#281)
    Based on PR to template app:
    https://github.com/ministryofjustice/hmpps-template-typescript/pull/94

--- integration_tests/mockApis/wiremock.ts
+++ integration_tests/mockApis/wiremock.ts
@@ -5,8 +5,6 @@ const url = 'http://localhost:9091/__admin'
 const stubFor = (mapping: Record<string, unknown>): SuperAgentRequest =>
   superagent.post(`${url}/mappings`).send(mapping)
 
-const getRequests = (): SuperAgentRequest => superagent.get(`${url}/requests`)
-
 const getMatchingRequests = body => superagent.post(`${url}/requests/find`).send(body)
 
 const resetStubs = (): Promise<Array<Response>> =>
@@ -28,4 +26,4 @@ const postFor = ({ body, urlPattern, urlPath }) =>
     },
   })
 
-export { stubFor, getMatchingRequests, getRequests, resetStubs, postFor }
+export { stubFor, getMatchingRequests, resetStubs, postFor }
