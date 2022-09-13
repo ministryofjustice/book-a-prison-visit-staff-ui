85b54f57738b5bb8f76cdcbff90059bc5ff36451
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-09-13 15:56:31 +0100

    Add Change a visit tile to feature flag

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,12 +2,16 @@ import type { RequestHandler, Router } from 'express'
 
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import { clearSession } from './visitorUtils'
+import config from '../config'
 
 export default function routes(router: Router): Router {
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/', (req, res, next) => {
-    res.render('pages/index', { hidePhaseBanner: true })
+    res.render('pages/index', {
+      hidePhaseBanner: true,
+      updateJourneyEnabled: config.features.updateJourneyEnabled,
+    })
   })
 
   get('/back-to-start', (req, res) => {
