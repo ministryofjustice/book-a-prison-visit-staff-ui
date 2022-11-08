e1c5eab1f464969501b9724967b2f166f65b4a2a
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2022-11-08 12:43:09 +0000

    VB-1383, improve tests for GET route

--- server/routes/changeEstablishment.test.ts
+++ server/routes/changeEstablishment.test.ts
@@ -1,5 +1,7 @@
 import type { Express } from 'express'
 import request from 'supertest'
+import * as cheerio from 'cheerio'
+import { SessionData } from 'express-session'
 import { appWithAllRoutes } from './testutils/appSetup'
 
 let app: Express
@@ -13,25 +15,47 @@ afterEach(() => {
 })
 
 // const enabledPrisons = [
-//   { prisonId: 'HEI', name: 'Hewell (HMP)' },
-//   { prisonId: 'BLI', name: 'Bristol (HMP)' },
+//   { prisonId: 'HEI', prisonName: 'Hewell (HMP)' },
+//   { prisonId: 'BLI', prisonName: 'Bristol (HMP)' },
 // ]
+
 // const selectedEstablishment = {
-//   name: 'Bristol (HMP',
+//   name: 'Bristol (HMP)',
 //   prisonId: 'BLI',
 // }
 
 describe('GET /change-establishment', () => {
-  it('should render select establishment page', () => {
+  it('should render select establishment page, with default establishment selected', () => {
     app = appWithAllRoutes({})
 
     return request(app)
       .get('/change-establishment')
       .expect('Content-Type', /html/)
       .expect(res => {
-        expect(res.text).toContain('Manage prison visits - Select Establishment')
-        expect(res.text).toContain('Hewell (HMP)')
-        expect(res.text).toContain('Bristol (HMP)')
+        const $ = cheerio.load(res.text)
+        expect($('h1').text()).toBe('Select establishment')
+        expect($('input[name="establishment"]').eq(0).prop('value')).toBe('HEI')
+        expect($('input[name="establishment"]').eq(0).prop('checked')).toBe(true)
+        expect($('input[name="establishment"]').eq(1).prop('value')).toBe('BLI')
+        expect($('input[name="establishment"]').length).toBe(2)
+      })
+  })
+
+  it('should render select establishment page, with current establishment selected', () => {
+    app = appWithAllRoutes({
+      sessionData: { selectedEstablishment: { prisonId: 'BLI', prisonName: 'Bristol (HMP)' } } as SessionData,
+    })
+
+    return request(app)
+      .get('/change-establishment')
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        const $ = cheerio.load(res.text)
+        expect($('h1').text()).toBe('Select establishment')
+        expect($('input[name="establishment"]').eq(0).prop('value')).toBe('HEI')
+        expect($('input[name="establishment"]').eq(1).prop('value')).toBe('BLI')
+        expect($('input[name="establishment"]').eq(1).prop('checked')).toBe(true)
+        expect($('input[name="establishment"]').length).toBe(2)
       })
   })
 })
