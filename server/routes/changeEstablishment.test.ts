d35c5a2f0520d8fd9f3f745bb7d847e8f328ff33
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2022-11-08 16:35:35 +0000

    VB-1383, tests added for POST, happy path test needs work

--- server/routes/changeEstablishment.test.ts
+++ server/routes/changeEstablishment.test.ts
@@ -3,6 +3,8 @@ import request from 'supertest'
 import * as cheerio from 'cheerio'
 import { SessionData } from 'express-session'
 import { appWithAllRoutes } from './testutils/appSetup'
+import * as visitorUtils from './visitorUtils'
+// import { Prison } from '../@types/bapv'
 
 let app: Express
 
@@ -56,6 +58,58 @@ describe('GET /change-establishment', () => {
         expect($('input[name="establishment"]').eq(1).prop('value')).toBe('BLI')
         expect($('input[name="establishment"]').eq(1).prop('checked')).toBe(true)
         expect($('input[name="establishment"]').length).toBe(2)
+      })
+  })
+})
+
+describe('POST /change-establishment', () => {
+  it('should set validation errors if no establishment selected', () => {
+    jest.spyOn(visitorUtils, 'clearSession')
+    const sessionData = {
+      sessionData: { selectedEstablishment: { prisonId: 'BLI', prisonName: 'Bristol (HMP)' } } as SessionData,
+    }
+    app = appWithAllRoutes(sessionData)
+    return request(app)
+      .post(`/change-establishment`)
+      .send('establishment=')
+      .expect(302)
+      .expect('location', `/change-establishment`)
+      .expect(() => {
+        expect(visitorUtils.clearSession).toHaveBeenCalledTimes(0)
+      })
+  })
+
+  it('should set validation errors if invalid establishment selected', () => {
+    jest.spyOn(visitorUtils, 'clearSession')
+    const sessionData = {
+      sessionData: { selectedEstablishment: { prisonId: 'BLI', prisonName: 'Bristol (HMP)' } } as SessionData,
+    }
+    app = appWithAllRoutes(sessionData)
+    return request(app)
+      .post(`/change-establishment`)
+      .send('establishment=HEX')
+      .expect(302)
+      .expect('location', `/change-establishment`)
+      .expect(() => {
+        expect(visitorUtils.clearSession).toHaveBeenCalledTimes(0)
+      })
+  })
+
+  it('should clear session, set selected establishment and redirect to home page', () => {
+    jest.spyOn(visitorUtils, 'clearSession')
+    const sessionData = {
+      sessionData: { selectedEstablishment: { prisonId: 'BLI', prisonName: 'Bristol (HMP)' } } as SessionData,
+    }
+
+    app = appWithAllRoutes(sessionData)
+    return request(app)
+      .post(`/change-establishment`)
+      .send('establishment=HEI')
+      .expect(302)
+      .expect('location', `/`)
+      .expect(() => {
+        // expect(sessionData).toBe('')
+        expect(visitorUtils.clearSession).toHaveBeenCalledTimes(1)
       })
   })
 })
