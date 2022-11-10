92820713917a1d5ef76b70b9ff7cbc0265e39a5c
hutcheonb-moj <106151419+hutcheonb-moj@users.noreply.github.com>
2022-11-10 14:13:57 +0000

    VB-1383, Select Establishment post and tests (#405)
    * VB-1383, added initial GET test
    
    * VB-1383, added clear session
    
    * VB-1383, improve tests for GET route
    
    * VB-1383, tests added for POST, happy path test needs work
    
    * VB-1383, added flash error to POST tests
    
    * VB-1383, merged with main, incorporated supported prisons service
    
    * VB-1383, wrapped page in feature flag
    
    * Added test coverage for feature flag
    
    * VB-1383, improved POST tests

---
+++ server/routes/changeEstablishment.test.ts
@@ -0,0 +1,143 @@
+import type { Express } from 'express'
+import request from 'supertest'
+import * as cheerio from 'cheerio'
+import { SessionData } from 'express-session'
+import config from '../config'
+import { appWithAllRoutes, flashProvider } from './testutils/appSetup'
+import * as visitorUtils from './visitorUtils'
+import SupportedPrisonsService from '../services/supportedPrisonsService'
+
+jest.mock('../services/supportedPrisonsService')
+
+let app: Express
+const systemToken = async (user: string): Promise<string> => `${user}-token-1`
+
+const supportedPrisonsService = new SupportedPrisonsService(null, systemToken) as jest.Mocked<SupportedPrisonsService>
+
+const supportedPrisons = [
+  { prisonId: 'HEI', prisonName: 'Hewell (HMP)' },
+  { prisonId: 'BLI', prisonName: 'Bristol (HMP)' },
+]
+
+beforeEach(() => {
+  supportedPrisonsService.getSupportedPrisons.mockResolvedValue(supportedPrisons)
+  config.features.establishmentSwitcherEnabled = true
+})
+
+afterEach(() => {
+  jest.resetAllMocks()
+})
+
+describe('GET /change-establishment', () => {
+  it('should render select establishment page, with default establishment selected', () => {
+    app = appWithAllRoutes({
+      supportedPrisonsServiceOverride: supportedPrisonsService,
+      systemTokenOverride: systemToken,
+    })
+
+    return request(app)
+      .get('/change-establishment')
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        const $ = cheerio.load(res.text)
+        expect($('h1').text()).toBe('Select establishment')
+        expect($('input[name="establishment"]').eq(0).prop('value')).toBe('HEI')
+        expect($('input[name="establishment"]').eq(0).prop('checked')).toBe(true)
+        expect($('input[name="establishment"]').eq(1).prop('value')).toBe('BLI')
+        expect($('input[name="establishment"]').length).toBe(2)
+      })
+  })
+
+  it('should render select establishment page, with current establishment selected', () => {
+    app = appWithAllRoutes({
+      supportedPrisonsServiceOverride: supportedPrisonsService,
+      systemTokenOverride: systemToken,
+      sessionData: { selectedEstablishment: supportedPrisons[1] } as SessionData,
+    })
+
+    return request(app)
+      .get('/change-establishment')
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        const $ = cheerio.load(res.text)
+        expect($('h1').text()).toBe('Select establishment')
+        expect($('input[name="establishment"]').eq(0).prop('value')).toBe('HEI')
+        expect($('input[name="establishment"]').eq(1).prop('value')).toBe('BLI')
+        expect($('input[name="establishment"]').eq(1).prop('checked')).toBe(true)
+        expect($('input[name="establishment"]').length).toBe(2)
+      })
+  })
+
+  it('should not render select establishment page, when feature flag disabled', () => {
+    config.features.establishmentSwitcherEnabled = false
+    app = appWithAllRoutes({
+      supportedPrisonsServiceOverride: supportedPrisonsService,
+      systemTokenOverride: systemToken,
+    })
+
+    return request(app).get('/change-establishment').expect(404)
+  })
+})
+
+describe('POST /change-establishment', () => {
+  let sessionData: SessionData
+
+  beforeEach(() => {
+    jest.spyOn(visitorUtils, 'clearSession')
+    sessionData = { selectedEstablishment: supportedPrisons[1] } as SessionData
+
+    app = appWithAllRoutes({
+      supportedPrisonsServiceOverride: supportedPrisonsService,
+      systemTokenOverride: systemToken,
+      sessionData,
+    })
+  })
+
+  it('should set validation errors if no establishment selected', () => {
+    return request(app)
+      .post(`/change-establishment`)
+      .send('establishment=')
+      .expect(302)
+      .expect('location', `/change-establishment`)
+      .expect(() => {
+        expect(sessionData.selectedEstablishment).toStrictEqual(supportedPrisons[1])
+        expect(flashProvider).toHaveBeenCalledWith('errors', [
+          { location: 'body', msg: 'No prison selected', param: 'establishment', value: '' },
+        ])
+        expect(visitorUtils.clearSession).toHaveBeenCalledTimes(0)
+      })
+  })
+
+  it('should set validation errors if invalid establishment selected', () => {
+    return request(app)
+      .post(`/change-establishment`)
+      .send('establishment=HEX')
+      .expect(302)
+      .expect('location', `/change-establishment`)
+      .expect(() => {
+        expect(sessionData.selectedEstablishment).toStrictEqual(supportedPrisons[1])
+        expect(flashProvider).toHaveBeenCalledWith('errors', [
+          { location: 'body', msg: 'No prison selected', param: 'establishment', value: 'HEX' },
+        ])
+        expect(visitorUtils.clearSession).toHaveBeenCalledTimes(0)
+      })
+  })
+
+  it('should clear session, set selected establishment and redirect to home page', () => {
+    return request(app)
+      .post(`/change-establishment`)
+      .send('establishment=HEI')
+      .expect(302)
+      .expect('location', `/`)
+      .expect(() => {
+        expect(sessionData.selectedEstablishment).toStrictEqual(supportedPrisons[0])
+        expect(visitorUtils.clearSession).toHaveBeenCalledTimes(1)
+      })
+  })
+
+  it('should not post if feature flag is disabled', () => {
+    config.features.establishmentSwitcherEnabled = false
+
+    return request(app).post(`/change-establishment`).send('establishment=HEI').expect(404)
+  })
+})
--- server/routes/changeEstablishment.ts
+++ server/routes/changeEstablishment.ts
@@ -1,12 +1,20 @@
 import type { NextFunction, RequestHandler, Router } from 'express'
 import { NotFound } from 'http-errors'
+import { body, validationResult } from 'express-validator'
 import config from '../config'
+import { Prison } from '../@types/bapv'
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import SupportedPrisonsService from '../services/supportedPrisonsService'
+import { clearSession } from './visitorUtils'
 
 export default function routes(router: Router, supportedPrisonsService: SupportedPrisonsService): Router {
   const get = (path: string, ...handlers: RequestHandler[]) =>
     router.get(
+      path,
+      handlers.map(handler => asyncMiddleware(handler)),
+    )
+  const post = (path: string, ...handlers: RequestHandler[]) =>
+    router.post(
       path,
       handlers.map(handler => asyncMiddleware(handler)),
     )
@@ -15,9 +23,32 @@ export default function routes(router: Router, supportedPrisonsService: Supporte
     const supportedPrisons = await supportedPrisonsService.getSupportedPrisons(res.locals.user?.username)
 
     res.render('pages/changeEstablishment', {
+      errors: req.flash('errors'),
       supportedPrisons,
     })
   })
+
+  post('/', establishmentSwitcherCheckMiddleware, async (req, res) => {
+    const supportedPrisons = await supportedPrisonsService.getSupportedPrisons(res.locals.user?.username)
+    await body('establishment').isIn(getPrisonIds(supportedPrisons)).withMessage('No prison selected').run(req)
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      req.flash('errors', errors.array() as [])
+      return res.redirect(req.originalUrl)
+    }
+
+    clearSession(req)
+
+    const newEstablishment = supportedPrisons.find(prison => prison.prisonId === req.body.establishment)
+    req.session.selectedEstablishment = Object.assign(req.session.selectedEstablishment ?? {}, newEstablishment)
+
+    return res.redirect('/')
+  })
+
+  function getPrisonIds(prisons: Prison[]) {
+    return prisons.map(prison => prison.prisonId)
+  }
   return router
 }
 
