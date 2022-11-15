0e4229e7f4ba69e40f5e8fc59c2914cc9d4a4082
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2022-11-15 08:01:06 +0000

    Add mock Prison Register API for integration tests

---
+++ integration_tests/mockApis/prisonRegister.ts
@@ -0,0 +1,19 @@
+import { SuperAgentRequest } from 'superagent'
+import { stubFor } from './wiremock'
+import allPrisons from './responses/allPrisons'
+
+export default {
+  stubGetPrisons: (prisons: Record<string, string>[] = allPrisons): SuperAgentRequest => {
+    return stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/prisonRegister/prisons',
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: prisons,
+      },
+    })
+  },
+}
