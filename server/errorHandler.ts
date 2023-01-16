0560e97625fe6c977e2fef467d76e9866f006244
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2023-01-16 15:22:50 +0000

    VB-1458, error message in production - 404

--- server/errorHandler.ts
+++ server/errorHandler.ts
@@ -11,11 +11,16 @@ export default function createErrorHandler(production: boolean) {
       return res.redirect('/sign-out')
     }
 
+    const prodHeading = error.status === 404 ? 'Page not found' : 'Error'
     const prodMessage =
       error.status === 404
-        ? 'The page you were looking for could not be found. Please check the address and try again.'
-        : 'Something went wrong. The error has been logged. Please try again'
+        ? [
+            'If you typed the web address, check it is correct.',
+            'If you pasted the web address, check you copied the entire address.',
+          ]
+        : ['Something went wrong. The error has been logged. Please try again']
 
+    res.locals.heading = production ? prodHeading : null
     res.locals.message = production ? prodMessage : error.message
     res.locals.status = error.status
     res.locals.stack = production ? null : error.stack
