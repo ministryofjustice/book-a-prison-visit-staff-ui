6d04ad248c5d20d4391eed2bb8f88f764ebc32aa
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2023-01-30 11:19:41 +0000

    VB-1740: Remove hard-coded session capacity (#479)

--- server/constants/prisonConfiguration.test.ts
+++ server/constants/prisonConfiguration.test.ts
@@ -6,8 +6,6 @@ describe('Prison configuration', () => {
 
     expect(prisonConfiguration.prisonPhoneNumber).toBe('0300 060 6503')
     expect(prisonConfiguration.selectVisitorsText.length).toBe(2)
-    expect(prisonConfiguration.visitCapacity.open).toBe(30)
-    expect(prisonConfiguration.visitCapacity.closed).toBe(3)
   })
 
   it('should return default prison configuration when none available for requested prisonId', () => {
@@ -15,7 +13,5 @@ describe('Prison configuration', () => {
 
     expect(prisonConfiguration.prisonPhoneNumber).toBe('')
     expect(prisonConfiguration.selectVisitorsText.length).toBe(0)
-    expect(prisonConfiguration.visitCapacity.open).toBe(null)
-    expect(prisonConfiguration.visitCapacity.closed).toBe(null)
   })
 })
--- server/constants/prisonConfiguration.ts
+++ server/constants/prisonConfiguration.ts
@@ -1,10 +1,6 @@
 type PrisonConfiguration = {
   prisonPhoneNumber: string
   selectVisitorsText: string[]
-  visitCapacity: {
-    open: number
-    closed: number
-  }
 }
 
 const prisonConfiguration: Record<string, PrisonConfiguration> = {
@@ -14,10 +10,6 @@ const prisonConfiguration: Record<string, PrisonConfiguration> = {
       'Add up to 3 adults (aged 18 or older). Children can also be added to the visit.',
       'Contact HMP Bristol when the total number of visitors (adults and children) is more than 3.',
     ],
-    visitCapacity: {
-      open: 20,
-      closed: 1,
-    },
   },
   HEI: {
     prisonPhoneNumber: '0300 060 6503',
@@ -25,20 +17,33 @@ const prisonConfiguration: Record<string, PrisonConfiguration> = {
       'Add up to 3 people aged 10 and over, and 4 children under 10 years old.',
       'At least one visitor must be 18 or older.',
     ],
-    visitCapacity: {
-      open: 30,
-      closed: 3,
-    },
+  },
+  CFI: {
+    prisonPhoneNumber: '0300 303 2301',
+    selectVisitorsText: [
+      'You can add up to 3 people aged 16 and over, and 3 children under 16 years old.',
+      'At least one visitor must be 18 or older.',
+    ],
+  },
+  PNI: {
+    prisonPhoneNumber: '0300 058 8224',
+    selectVisitorsText: [
+      'You can add up to 3 people aged 16 and over, and 3 children under 16 years old.',
+      'At least one visitor must be 18 or older.',
+    ],
+  },
+  WWI: {
+    prisonPhoneNumber: '0300 060 6509',
+    selectVisitorsText: [
+      'You can add up to 3 people aged 18 and over, and 3 children under 18 years old.',
+      'At least one visitor must be 18 or older.',
+    ],
   },
 }
 
 const defaultConfiguration: PrisonConfiguration = {
   prisonPhoneNumber: '',
   selectVisitorsText: [],
-  visitCapacity: {
-    open: null,
-    closed: null,
-  },
 }
 
 const getPrisonConfiguration = (prisonId: string): PrisonConfiguration => {
