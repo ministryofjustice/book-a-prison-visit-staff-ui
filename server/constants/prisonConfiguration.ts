02bde576ffdd49f561fbbea50793c8176d7ab341
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2023-01-30 11:37:23 +0000

    Fix merge

--- server/constants/prisonConfiguration.ts
+++ server/constants/prisonConfiguration.ts
@@ -39,39 +39,6 @@ const prisonConfiguration: Record<string, PrisonConfiguration> = {
       'At least one visitor must be 18 or older.',
     ],
   },
-  CFI: {
-    prisonPhoneNumber: '0300 303 2301',
-    selectVisitorsText: [
-      'You can add up to 3 people aged 16 and over, and 3 children under 16 years old.',
-      'At least one visitor must be 18 or older.',
-    ],
-    visitCapacity: {
-      open: 35,
-      closed: 2,
-    },
-  },
-  PNI: {
-    prisonPhoneNumber: '0300 058 8224',
-    selectVisitorsText: [
-      'You can add up to 3 people aged 16 and over, and 3 children under 16 years old.',
-      'At least one visitor must be 18 or older.',
-    ],
-    visitCapacity: {
-      open: 32,
-      closed: 8,
-    },
-  },
-  WWI: {
-    prisonPhoneNumber: '0300 060 6509',
-    selectVisitorsText: [
-      'You can add up to 3 people aged 18 and over, and 3 children under 18 years old.',
-      'At least one visitor must be 18 or older.',
-    ],
-    visitCapacity: {
-      open: 28,
-      closed: 2,
-    },
-  },
 }
 
 const defaultConfiguration: PrisonConfiguration = {
