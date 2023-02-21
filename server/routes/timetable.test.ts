e2ad6d0e5789c88294c0b3c7676b4c47c5da95a8
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2023-02-21 11:25:32 +0000

    Add timtable page tests

--- server/routes/timetable.test.ts
+++ server/routes/timetable.test.ts
@@ -14,9 +14,12 @@ beforeEach(() => {
 
 afterEach(() => {
   jest.resetAllMocks()
+  jest.useRealTimers()
 })
 
 describe('View visits timetable', () => {
+  let fakeDate: string
+
   it('should return a 404 if the feature is disabled', () => {
     config.features.viewTimetableEnabled = false
     app = appWithAllRoutes({})
@@ -24,8 +27,9 @@ describe('View visits timetable', () => {
     return request(app).get('/timetable').expect(404)
   })
 
-  it('should render the visits timetable page', () => {
-    app = appWithAllRoutes({})
+  it('should render the visits timetable page with closest Monday selected by default', () => {
+    fakeDate = '2022-12-27' // a Tuesday
+    jest.useFakeTimers({ advanceTimers: true, now: new Date(fakeDate) })
 
     return request(app)
       .get('/timetable')
@@ -35,8 +39,77 @@ describe('View visits timetable', () => {
 
         expect($('h1').text()).toBe('Visits timetable')
 
+        expect($('#selected-date').text()).toBe('Monday 26 December 2022')
+
+        expect($('.bapv-timetable-dates__date--selected').text().trim()).toMatch(/Mon\s+26 December/)
+        expect($('.bapv-timetable-dates__date a').eq(0).attr('href')).toBe('/timetable?date=2022-12-27')
+        expect($('.bapv-timetable-dates__date a').eq(1).attr('href')).toBe('/timetable?date=2022-12-28')
+        expect($('.bapv-timetable-dates__date a').eq(2).attr('href')).toBe('/timetable?date=2022-12-29')
+        expect($('.bapv-timetable-dates__date a').eq(3).attr('href')).toBe('/timetable?date=2022-12-30')
+        expect($('.bapv-timetable-dates__date a').eq(4).attr('href')).toBe('/timetable?date=2022-12-31')
+        expect($('.bapv-timetable-dates__date a').eq(5).attr('href')).toBe('/timetable?date=2023-01-01')
+
+        expect($('[data-test="previous-week"]').attr('href')).toBe('/timetable?date=2022-12-19')
+        expect($('[data-test="next-week"]').attr('href')).toBe('/timetable?date=2023-01-02')
+
         expect($('[data-test="change-timetable"]').text()).toBe('Request changes to the timetable')
         expect($('[data-test="change-request"]').attr('href')).toBe('LINK_TBC')
+      })
+  })
+
+  it('should render the visits timetable page with requested date selected', () => {
+    fakeDate = '2022-12-30' // a Friday
+    jest.useFakeTimers({ advanceTimers: true, now: new Date(fakeDate) })
+
+    return request(app)
+      .get('/timetable')
+      .query({ date: fakeDate })
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        const $ = cheerio.load(res.text)
+
+        expect($('h1').text()).toBe('Visits timetable')
+
+        expect($('#selected-date').text()).toBe('Friday 30 December 2022')
+
+        expect($('.bapv-timetable-dates__date a').eq(0).attr('href')).toBe('/timetable?date=2022-12-26')
+        expect($('.bapv-timetable-dates__date a').eq(1).attr('href')).toBe('/timetable?date=2022-12-27')
+        expect($('.bapv-timetable-dates__date a').eq(2).attr('href')).toBe('/timetable?date=2022-12-28')
+        expect($('.bapv-timetable-dates__date a').eq(3).attr('href')).toBe('/timetable?date=2022-12-29')
+        expect($('.bapv-timetable-dates__date--selected').text().trim()).toMatch(/Fri\s+30 December/)
+        expect($('.bapv-timetable-dates__date a').eq(4).attr('href')).toBe('/timetable?date=2022-12-31')
+        expect($('.bapv-timetable-dates__date a').eq(5).attr('href')).toBe('/timetable?date=2023-01-01')
+
+        expect($('[data-test="previous-week"]').attr('href')).toBe('/timetable?date=2022-12-19')
+        expect($('[data-test="next-week"]').attr('href')).toBe('/timetable?date=2023-01-02')
+      })
+  })
+
+  it('should render the visits timetable page with closest Monday selected when invalid date requested', () => {
+    fakeDate = '2022-12-26' // a Monday
+    jest.useFakeTimers({ advanceTimers: true, now: new Date(fakeDate) })
+
+    return request(app)
+      .get('/timetable')
+      .query({ date: 'NOT A DATE!' })
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        const $ = cheerio.load(res.text)
+
+        expect($('h1').text()).toBe('Visits timetable')
+
+        expect($('#selected-date').text()).toBe('Monday 26 December 2022')
+
+        expect($('.bapv-timetable-dates__date--selected').text().trim()).toMatch(/Mon\s+26 December/)
+        expect($('.bapv-timetable-dates__date a').eq(0).attr('href')).toBe('/timetable?date=2022-12-27')
+        expect($('.bapv-timetable-dates__date a').eq(1).attr('href')).toBe('/timetable?date=2022-12-28')
+        expect($('.bapv-timetable-dates__date a').eq(2).attr('href')).toBe('/timetable?date=2022-12-29')
+        expect($('.bapv-timetable-dates__date a').eq(3).attr('href')).toBe('/timetable?date=2022-12-30')
+        expect($('.bapv-timetable-dates__date a').eq(4).attr('href')).toBe('/timetable?date=2022-12-31')
+        expect($('.bapv-timetable-dates__date a').eq(5).attr('href')).toBe('/timetable?date=2023-01-01')
+
+        expect($('[data-test="previous-week"]').attr('href')).toBe('/timetable?date=2022-12-19')
+        expect($('[data-test="next-week"]').attr('href')).toBe('/timetable?date=2023-01-02')
       })
   })
 })
