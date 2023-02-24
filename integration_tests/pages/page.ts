7fd8a40c448bcbbdfedea715809fd5f01b2187f6
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2023-02-24 07:31:48 +0000

    Add verifyOnPageTitle() to Page class

--- integration_tests/pages/page.ts
+++ integration_tests/pages/page.ts
@@ -5,6 +5,10 @@ export default abstract class Page {
     return new constructor()
   }
 
+  static verifyOnPageTitle = <T>(constructor: new (string) => T, title?: string): T => {
+    return new constructor(title)
+  }
+
   constructor(private readonly title: string) {
     this.checkOnPage()
   }
