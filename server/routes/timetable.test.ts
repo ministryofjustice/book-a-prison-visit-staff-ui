7697c478217ef93cad03ebd6bbf7738cc0daa370
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2023-03-01 10:41:17 +0000

    Update request change timetable link

--- server/routes/timetable.test.ts
+++ server/routes/timetable.test.ts
@@ -70,7 +70,7 @@ describe('View visits timetable', () => {
 
         expect($('[data-test="change-timetable"]').text()).toBe('Request changes to the timetable')
         expect($('[data-test="change-request"]').attr('href')).toBe(
-          'https://request-visit-schedule-change.form.service.justice.gov.uk',
+          'https://request-changes-to-the-visits-timetable.form.service.justice.gov.uk/',
         )
 
         expect(visitSessionsService.getSessionSchedule).toHaveBeenCalledWith({
