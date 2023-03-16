2f114d9e108ec80d7151dc022157f6e24ec0b5c0
hutcheonb-moj <106151419+hutcheonb-moj@users.noreply.github.com>
2023-03-16 07:35:40 +0000

    VB-1883, cancel visit journey integration tests (#527)

---
+++ integration_tests/pages/visitCancelled.ts
@@ -0,0 +1,11 @@
+import Page, { PageElement } from './page'
+
+export default class VisitCancelledPage extends Page {
+  constructor() {
+    super('Booking cancelled')
+  }
+
+  visitDetails = (): PageElement => cy.get('[data-test="visit-details"]')
+
+  homeButton = (): PageElement => cy.get('[data-test="go-to-start"]')
+}
