f11218e7cb01cb3551a5ffa413fd459799d6bf3c
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2023-05-23 15:04:55 +0100

    Add integration test for check visit page

---
+++ integration_tests/integration/checkYourBooking.cy.ts
@@ -0,0 +1,227 @@
+import { addDays, format, sub } from 'date-fns'
+import Page from '../pages/page'
+import PrisonerProfilePage from '../pages/prisonerProfile'
+import SelectVisitorsPage from '../pages/selectVisitors'
+import TestData from '../../server/routes/testutils/testData'
+import { VisitSession } from '../../server/data/orchestrationApiTypes'
+import AdditionalSupportPage from '../pages/additionalSupport'
+import MainContactPage from '../pages/mainContact'
+import CheckYourBookingPage from '../pages/checkYourBooking'
+import ConfirmationPage from '../pages/confirmation'
+import SelectVisitDateAndTime from '../pages/selectVisitDateAndTime'
+
+context('Check visit details page', () => {
+  const shortDateFormat = 'yyyy-MM-dd'
+  const longDateFormat = 'EEEE d MMMM yyyy'
+
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.task('stubAuthUser')
+    cy.task('stubSupportedPrisonIds')
+    cy.task('stubPrisons')
+    cy.signIn()
+  })
+
+  it('should complete the book a visit journey and change details before booking', () => {
+    const prisonerProfile = TestData.prisonerProfile()
+    const { prisonerId, prisonId } = prisonerProfile
+    const prisonerDisplayName = 'Smith, John'
+
+    const today = new Date()
+    const childDob = format(sub(today, { years: 5 }), shortDateFormat)
+    const contacts = [
+      TestData.contact(),
+      TestData.contact({
+        personId: 4322,
+        firstName: 'Bob',
+        dateOfBirth: childDob,
+        relationshipCode: 'SON',
+        relationshipDescription: 'Son',
+      }),
+    ]
+
+    const visitSessions: VisitSession[] = [
+      TestData.visitSession({
+        startTimestamp: format(addDays(today, 7), `${shortDateFormat}'T'10:00:00`),
+        endTimestamp: format(addDays(today, 7), `${shortDateFormat}'T'11:00:00`),
+      }),
+      TestData.visitSession({
+        startTimestamp: format(addDays(today, 8), `${shortDateFormat}'T'13:30:00`),
+        endTimestamp: format(addDays(today, 8), `${shortDateFormat}'T'15:00:00`),
+      }),
+    ]
+
+    // Start - Prisoner profile page
+    cy.task('stubPrisonerSocialContacts', { offenderNo: prisonerId, contacts })
+    cy.task('stubPrisonerProfile', { prisonId, prisonerId, profile: prisonerProfile })
+    cy.visit(`/prisoner/${prisonerId}`)
+
+    const prisonerProfilePage = Page.verifyOnPageTitle(PrisonerProfilePage, prisonerDisplayName)
+
+    // Select visitors - first of two
+    cy.task('stubOffenderRestrictions', { offenderNo: prisonerId, offenderRestrictions: [] })
+    prisonerProfilePage.bookAVisitButton().click()
+    const selectVisitorsPage = Page.verifyOnPage(SelectVisitorsPage)
+    selectVisitorsPage.getVisitor(contacts[0].personId).check()
+
+    // Select date and time
+    cy.task('stubVisitSessions', {
+      offenderNo: prisonerId,
+      prisonId,
+      visitSessions,
+    })
+    cy.task('stubOffenderEvents', {
+      offenderNo: prisonerId,
+      fromDate: format(today, shortDateFormat),
+      toDate: format(addDays(today, 8), shortDateFormat),
+      scheduledEvents: [],
+    })
+    cy.task(
+      'stubReserveVisit',
+      TestData.visit({
+        visitStatus: 'RESERVED',
+        startTimestamp: visitSessions[0].startTimestamp,
+        endTimestamp: visitSessions[0].endTimestamp,
+        visitors: [{ nomisPersonId: contacts[0].personId }],
+      }),
+    )
+    selectVisitorsPage.continueButton().click()
+    const selectVisitDateAndTime = Page.verifyOnPage(SelectVisitDateAndTime)
+    selectVisitDateAndTime.expandAllSections()
+    selectVisitDateAndTime.getSlotById(1).check()
+
+    // Additional support
+    cy.task('stubAvailableSupport')
+    selectVisitDateAndTime.continueButton().click()
+    const additionalSupportPage = Page.verifyOnPage(AdditionalSupportPage)
+    additionalSupportPage.additionalSupportNotRequired().check()
+
+    // Main contact
+    additionalSupportPage.continueButton().click()
+    const mainContactPage = Page.verifyOnPage(MainContactPage)
+    mainContactPage.getFirstContact().check()
+    mainContactPage.enterPhoneNumber('01234 567890')
+
+    // Check visit details
+    mainContactPage.continueButton().click()
+    const checkYourBookingPage = Page.verifyOnPage(CheckYourBookingPage)
+    checkYourBookingPage.prisonerName().contains(prisonerDisplayName)
+    checkYourBookingPage.visitDate().contains(format(new Date(visitSessions[0].startTimestamp), longDateFormat))
+    checkYourBookingPage.visitTime().contains('10am to 11am')
+    checkYourBookingPage.visitType().contains('Open')
+    checkYourBookingPage.visitorName(1).contains('Jeanette Smith (wife of the prisoner)')
+    checkYourBookingPage.additionalSupport().contains('None')
+    checkYourBookingPage.mainContactName().contains('Jeanette Smith (wife of the prisoner)')
+    checkYourBookingPage.mainContactNumber().contains('01234 567890')
+
+    // Check details - change visit date - then proceed through journey
+    checkYourBookingPage.changeVisitDate().click()
+    Page.verifyOnPage(SelectVisitDateAndTime)
+    selectVisitDateAndTime.expandAllSections()
+    selectVisitDateAndTime.getSlotById(1).should('be.checked')
+    selectVisitDateAndTime.getSlotById(2).check()
+    cy.task(
+      'stubChangeReservedSlot',
+      TestData.visit({
+        visitStatus: 'RESERVED',
+        startTimestamp: visitSessions[1].startTimestamp,
+        endTimestamp: visitSessions[1].endTimestamp,
+        visitContact: { name: 'Jeanette Smith', telephone: '01234 567890' },
+        visitors: [{ nomisPersonId: contacts[0].personId, visitContact: true }],
+        visitorSupport: [],
+      }),
+    )
+    selectVisitDateAndTime.continueButton().click()
+    additionalSupportPage.continueButton().click()
+    mainContactPage.continueButton().click()
+    checkYourBookingPage.visitDate().contains(format(new Date(visitSessions[1].startTimestamp), longDateFormat))
+    checkYourBookingPage.visitTime().contains('1:30pm to 3pm')
+
+    // Check details - change visitors, add visitor - then proceed through journey
+    checkYourBookingPage.changeVisitors().click()
+    selectVisitorsPage.getVisitor(contacts[0].personId).should('be.checked')
+    selectVisitorsPage.getVisitor(contacts[1].personId).should('not.be.checked')
+    selectVisitorsPage.getVisitor(contacts[1].personId).check()
+    selectVisitorsPage.continueButton().click()
+    selectVisitDateAndTime.getSlotById(2).should('be.checked')
+    cy.task(
+      'stubChangeReservedSlot',
+      TestData.visit({
+        visitStatus: 'RESERVED',
+        startTimestamp: visitSessions[1].startTimestamp,
+        endTimestamp: visitSessions[1].endTimestamp,
+        visitContact: { name: 'Jeanette Smith', telephone: '01234 567890' },
+        visitors: [
+          { nomisPersonId: contacts[0].personId, visitContact: true },
+          { nomisPersonId: contacts[1].personId, visitContact: false },
+        ],
+        visitorSupport: [],
+      }),
+    )
+    selectVisitDateAndTime.continueButton().click()
+    additionalSupportPage.continueButton().click()
+    mainContactPage.continueButton().click()
+    checkYourBookingPage.visitorName(1).contains('Jeanette Smith (wife of the prisoner)')
+    checkYourBookingPage.visitorName(2).contains('Bob Smith (son of the prisoner)')
+
+    // Check details - change additional support - then proceed through journey
+    checkYourBookingPage.changeAdditionalSupport().click()
+    additionalSupportPage.additionalSupportNotRequired().should('be.checked')
+    additionalSupportPage.additionalSupportRequired().check()
+    additionalSupportPage.selectSupportType('WHEELCHAIR')
+    additionalSupportPage.continueButton().click()
+    mainContactPage.continueButton().click()
+    checkYourBookingPage.additionalSupport().contains('Wheelchair ramp')
+
+    // Check details - change main contact number - then proceed through journey
+    checkYourBookingPage.changeMainContact().click()
+    mainContactPage.enterPhoneNumber('09876 543 321')
+    mainContactPage.continueButton().click()
+    checkYourBookingPage.mainContactNumber().contains('09876 543 321')
+
+    // Confirmation
+    cy.task(
+      'stubChangeReservedSlot',
+      TestData.visit({
+        visitStatus: 'RESERVED',
+        startTimestamp: visitSessions[1].startTimestamp,
+        endTimestamp: visitSessions[1].endTimestamp,
+        visitContact: { name: 'Jeanette Smith', telephone: '09876 543 321' },
+        visitors: [
+          { nomisPersonId: contacts[0].personId, visitContact: true },
+          { nomisPersonId: contacts[1].personId, visitContact: false },
+        ],
+        visitorSupport: [{ type: 'WHEELCHAIR' }],
+      }),
+    )
+    cy.task(
+      'stubBookVisit',
+      TestData.visit({
+        visitStatus: 'BOOKED',
+        startTimestamp: visitSessions[1].startTimestamp,
+        endTimestamp: visitSessions[1].endTimestamp,
+        visitContact: { name: 'Jeanette Smith', telephone: '09876 543 321' },
+        visitors: [
+          { nomisPersonId: contacts[0].personId, visitContact: true },
+          { nomisPersonId: contacts[1].personId, visitContact: false },
+        ],
+        visitorSupport: [{ type: 'WHEELCHAIR' }],
+      }),
+    )
+
+    checkYourBookingPage.bookButton().click()
+    const confirmationPage = Page.verifyOnPageTitle(ConfirmationPage, 'Booking confirmed')
+    confirmationPage.bookingReference().contains(TestData.visit().reference)
+    confirmationPage.prisonerName().contains(prisonerDisplayName)
+    confirmationPage.prisonerNumber().contains(prisonerId)
+    confirmationPage.visitDate().contains(format(new Date(visitSessions[1].startTimestamp), longDateFormat))
+    confirmationPage.visitTime().contains('1:30pm to 3pm')
+    confirmationPage.visitType().contains('Open')
+    confirmationPage.visitorName(1).contains('Jeanette Smith (wife of the prisoner)')
+    confirmationPage.visitorName(2).contains('Bob Smith (son of the prisoner)')
+    confirmationPage.additionalSupport().contains('Wheelchair ramp')
+    confirmationPage.mainContactName().contains('Jeanette Smith (wife of the prisoner)')
+    confirmationPage.mainContactNumber().contains('09876 543 321')
+  })
+})
--- integration_tests/pages/checkYourBooking.ts
+++ integration_tests/pages/checkYourBooking.ts
@@ -9,17 +9,25 @@ export default class CheckYourBookingPage extends Page {
 
   visitDate = (): PageElement => cy.get('.test-visit-date')
 
+  changeVisitDate = (): PageElement => cy.get('[data-test="change-date"]')
+
   visitTime = (): PageElement => cy.get('.test-visit-time')
 
   visitType = (): PageElement => cy.get('.test-visit-type')
 
   visitorName = (number: number): PageElement => cy.get(`.test-visitor-name${number}`)
 
+  changeVisitors = (): PageElement => cy.get('[data-test="change-visitors"]')
+
   additionalSupport = (): PageElement => cy.get('.test-additional-support')
+
+  changeAdditionalSupport = (): PageElement => cy.get('[data-test="change-additional-support"]')
 
   mainContactName = (): PageElement => cy.get('.test-main-contact-name')
 
   mainContactNumber = (): PageElement => cy.get('.test-main-contact-number')
+
+  changeMainContact = (): PageElement => cy.get('[data-test="change-main-contact"]')
 
   bookButton = (): PageElement => cy.get('[data-test=submit]')
 }
