a517b1b14b49a54200a328b03d94657916dea3a5
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2023-07-05 12:31:39 +0100

    Merge develop into main (#613)
    * Revert "Revert "VB-2357 Support n-weekly schedule (#602)" (#605)" (#606)
    
    This reverts commit 4fee644c93492a89e5e384dfc4b3e9f92bcf5f1f.
    
    * Handle 'one off' sessions on timetable (#604)
    
    * VB-2312: Use new secret for redis (#607) (#609)
    
    * VB-2312: Use new secret for redis
    
    * Fix secrets
    
    * Move back to global secret
    
    Co-authored-by: Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
    
    * Update from main (#612)
    
    * VB-2312: Use new secret for redis (#607)
    
    * VB-2312: Use new secret for redis
    
    * Fix secrets
    
    * Move back to global secret
    
    * Fix semver security vuln (#610)
    
    ---------
    
    Co-authored-by: Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>

--- server/constants/sessionTemplateFrequency.ts
+++
@@ -1,9 +0,0 @@
-import { SessionSchedule } from '../data/orchestrationApiTypes'
-
-const sessionTemplateFrequency: Record<SessionSchedule['sessionTemplateFrequency'], string> = {
-  WEEKLY: 'Weekly',
-  BI_WEEKLY: 'Fortnightly',
-  ONE_OFF: 'One off',
-}
-
-export default sessionTemplateFrequency
