946d2102eeb46a92bf811782de3d790dc1307de1
hutcheonb-moj <106151419+hutcheonb-moj@users.noreply.github.com>
2023-08-03 11:59:20 +0100

    Have migrated event audit's show as booked/cancelled instead (#625)

--- server/constants/eventAuditTypes.ts
+++ server/constants/eventAuditTypes.ts
@@ -4,7 +4,6 @@ const eventAuditTypes: Partial<Record<EventAuditType, string>> = {
   BOOKED_VISIT: 'Visit booked',
   UPDATED_VISIT: 'Visit updated',
   CANCELLED_VISIT: 'Visit cancelled',
-  MIGRATED_VISIT: 'Visit booked', // Will be more correct (based on information given in this event) to show visit booked
 }
 
 export default eventAuditTypes
