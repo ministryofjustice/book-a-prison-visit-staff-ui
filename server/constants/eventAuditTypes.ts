19373e55b6897413fb440ac0c91a1f855f1e3f32
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2023-08-11 15:23:21 +0100

    VB-2671, display migrated visit against migrated events

--- server/constants/eventAuditTypes.ts
+++ server/constants/eventAuditTypes.ts
@@ -4,6 +4,7 @@ const eventAuditTypes: Partial<Record<EventAuditType, string>> = {
   BOOKED_VISIT: 'Visit booked',
   UPDATED_VISIT: 'Visit updated',
   CANCELLED_VISIT: 'Visit cancelled',
+  MIGRATED_VISIT: 'Visit migrated',
 }
 
 export default eventAuditTypes
