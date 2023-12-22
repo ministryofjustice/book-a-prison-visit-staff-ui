8903ca25decc509d588c13153079d9856c1f72be
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2023-12-22 15:33:01 +0000

    Update to GOV.UK Frontend v5 & MoJ v2beta

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -35,8 +35,8 @@ export function registerNunjucks(app?: express.Express): Environment {
   const njkEnv = nunjucks.configure(
     [
       path.join(__dirname, '../../server/views'),
-      'node_modules/govuk-frontend/',
-      'node_modules/govuk-frontend/components/',
+      'node_modules/govuk-frontend/dist/',
+      'node_modules/govuk-frontend/dist/components/',
       'node_modules/@ministryofjustice/frontend/',
       'node_modules/@ministryofjustice/frontend/moj/components/',
     ],
