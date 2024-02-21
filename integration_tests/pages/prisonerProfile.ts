901931576e2c5944c7e4245478fb66dd598a6f3d
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2024-02-21 09:43:07 +0000

    Fixed faulty tests - installing node packages changed tab naming

--- integration_tests/pages/prisonerProfile.ts
+++ integration_tests/pages/prisonerProfile.ts
@@ -27,11 +27,11 @@ export default class PrisonerProfilePage extends Page {
 
   bookAVisitButton = (): PageElement => cy.get('[data-test="book-a-visit"]')
 
-  selectVisitingOrdersTab = (): PageElement => cy.get('#tab_visiting-orders').click()
+  selectVisitingOrdersTab = (): PageElement => cy.get('#visiting-orders').click({ force: true })
 
-  selectActiveAlertsTab = (): PageElement => cy.get('#tab_active-alerts').click()
+  selectActiveAlertsTab = (): PageElement => cy.get('#active-alerts').click({ force: true })
 
-  selectVisitsTab = (): PageElement => cy.get('#tab_visits').click()
+  selectVisitsTab = (): PageElement => cy.get('#visits').click({ force: true })
 
   visitTabVORemaining = (): PageElement => cy.get('[data-test="tab-vo-remaining"]')
 
