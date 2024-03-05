9a60ce3ca252511534448ccbe36bf873881bd4cd
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2024-03-05 16:18:00 +0000

    Send no phone number if non provided

--- server/data/orchestrationApiClient.ts
+++ server/data/orchestrationApiClient.ts
@@ -220,7 +220,7 @@ export default class OrchestrationApiClient {
   } {
     const visitContact = mainContact
       ? {
-          telephone: mainContact.phoneNumber,
+          ...(mainContact.phoneNumber && { telephone: mainContact.phoneNumber }),
           name: mainContact.contactName ? mainContact.contactName : mainContact.contact.name,
         }
       : undefined
