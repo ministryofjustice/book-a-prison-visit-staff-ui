de205152def7620b2a71e86ca71308c7dc7a8c60
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-03-27 10:40:05 +0000

    Update GOVUK Frontend

--- integration_tests/pages/selectVisitDateAndTime.ts
+++ integration_tests/pages/selectVisitDateAndTime.ts
@@ -2,11 +2,7 @@ import Page, { PageElement } from './page'
 
 export default class SelectVisitDateAndTime extends Page {
   constructor() {
-    super('Select date and time of visit', {
-      // @TODO remove this ignore list once GOV.UK Frontend is released
-      // with accordion component changes in https://github.com/alphagov/govuk-frontend/pull/4628
-      axeRulesToIgnore: ['aria-prohibited-attr', 'aria-allowed-attr'],
-    })
+    super('Select date and time of visit')
   }
 
   expandAllSections = (): void => {
