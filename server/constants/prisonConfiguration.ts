8355212a768f9380360e7da48ace04aa122fb93e
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2024-04-08 10:47:00 +0100

    Update Send SDI visitor allowances (#745)

--- server/constants/prisonConfiguration.ts
+++ server/constants/prisonConfiguration.ts
@@ -257,7 +257,7 @@ const prisonConfiguration: Record<string, PrisonConfiguration> = {
   // Send (HMP)
   SDI: {
     selectVisitorsText: [
-      'You can add up to a total of 4 people, with a maximum of 4 people aged 18 and over.',
+      'You can add up to a total of 3 people, with a maximum of 3 people aged 18 and over.',
       'At least one visitor must be 18 or older.',
     ],
   },
