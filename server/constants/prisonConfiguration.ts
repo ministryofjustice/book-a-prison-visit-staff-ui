fa800e36022556ec12159584d094e68a3fb5e771
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2024-05-29 16:59:51 +0100

    Updated DMI visitor guidance
    Changed child age from 9 to 10

--- server/constants/prisonConfiguration.ts
+++ server/constants/prisonConfiguration.ts
@@ -83,7 +83,7 @@ const prisonConfiguration: Record<string, PrisonConfiguration> = {
   // Durham (HMP & YOI)
   DMI: {
     selectVisitorsText: [
-      'You can add up to 3 people aged 9 and over, and 3 children under 9 years old.',
+      'You can add up to 3 people aged 10 and over, and 3 children under 10 years old.',
       'At least one visitor must be 18 or older.',
     ],
   },
