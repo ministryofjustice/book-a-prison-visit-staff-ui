5bf19f5bd05f2556e74c2bf6185611d238beaccd
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2024-05-30 15:53:12 +0100

    Changed SDI visitor guidance (#764)
    Co-authored-by: Thomas McGowan <tpmcgowan@users.noreply.github.com>

--- server/constants/prisonConfiguration.ts
+++ server/constants/prisonConfiguration.ts
@@ -262,10 +262,7 @@ const prisonConfiguration: Record<string, PrisonConfiguration> = {
   },
   // Send (HMP)
   SDI: {
-    selectVisitorsText: [
-      'You can add up to a total of 3 people, with a maximum of 3 people aged 18 and over.',
-      'At least one visitor must be 18 or older.',
-    ],
+    selectVisitorsText: ['You can add up to a total of 4 people.', 'At least one visitor must be 18 or older.'],
   },
   // Stafford (HMP)
   SFI: {
