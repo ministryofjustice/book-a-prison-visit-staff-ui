47e0859e4b9eb5b45bd01fb631d34526ca6965d4
hutcheonb-moj <106151419+hutcheonb-moj@users.noreply.github.com>
2025-10-16 15:45:10 +0100

    VB-5996 Booker management: select booker account (#1120)

---
+++ server/routes/bookerManagement/selectBookerAccountController.test.ts
@@ -0,0 +1,137 @@
+import type { Express } from 'express'
+import request from 'supertest'
+import * as cheerio from 'cheerio'
+import { SessionData } from 'express-session'
+import { FieldValidationError } from 'express-validator'
+import { appWithAllRoutes, FlashData, flashProvider, user } from '../testutils/appSetup'
+import { createMockBookerService } from '../../services/testutils/mocks'
+import bapvUserRoles from '../../constants/bapvUserRoles'
+import TestData from '../testutils/testData'
+
+let app: Express
+let flashData: FlashData
+let sessionData: SessionData
+
+const bookerService = createMockBookerService()
+
+const url = '/manage-bookers/select-account'
+const booker1 = TestData.bookerSearchResult()
+const booker2 = TestData.bookerSearchResult({ reference: 'bbbb-cccc-dddd', createdTimestamp: '2024-10-09T12:00:00' })
+
+beforeEach(() => {
+  flashData = {}
+  flashProvider.mockImplementation((key: keyof FlashData) => flashData[key])
+
+  sessionData = {} as SessionData
+  app = appWithAllRoutes({
+    services: { bookerService },
+    userSupplier: () => ({ ...user, userRoles: [bapvUserRoles.BOOKER_ADMIN] }),
+    sessionData,
+  })
+})
+
+afterEach(() => {
+  jest.resetAllMocks()
+})
+
+describe('Booker management - select booker account when multiple accounts present', () => {
+  describe(`GET ${url}`, () => {
+    it('should require booker admin role', () => {
+      app = appWithAllRoutes({ services: { bookerService }, sessionData })
+      return request(app).get(url).expect(302).expect('location', '/authError')
+    })
+
+    it('should render select booker page when at least 2 accounts present', () => {
+      sessionData.matchedBookers = [booker1, booker2]
+
+      return request(app)
+        .get(url)
+        .expect('Content-Type', /html/)
+        .expect(res => {
+          const $ = cheerio.load(res.text)
+          // Page header
+          expect($('title').text()).toMatch(/^Select account to manage -/)
+          expect($('.govuk-back-link').attr('href')).toBe('/manage-bookers/search')
+          expect($('h1').text().trim()).toBe('Select account to manage')
+          expect($('[data-test=booker-email]').text()).toBe(booker1.email)
+
+          // Form
+          expect($('form').attr('action')).toBe(url)
+          expect($('input[name=reference]').eq(0).val()).toBe(booker1.reference)
+          expect($('label[for="reference"]').text().trim()).toBe('Created on 9 October 2025 at 12pm')
+          expect($('input[name=reference]').eq(1).val()).toBe(booker2.reference)
+          expect($('label[for="reference-2"]').text().trim()).toBe('Created on 9 October 2024 at 12pm')
+          expect($('[data-test=continue]').text().trim()).toBe('Continue')
+        })
+    })
+
+    it('should redirect to previous page, if only 1 booker account present', () => {
+      sessionData.matchedBookers = [booker1]
+
+      return request(app).get(url).expect(302).expect('location', '/manage-bookers/search')
+    })
+
+    it('should render validation error', () => {
+      sessionData.matchedBookers = [booker1, booker2]
+      const missingReference = 'missing-reference'
+      const validationError: FieldValidationError = {
+        type: 'field',
+        location: 'body',
+        path: 'reference',
+        value: missingReference,
+        msg: 'Select an account to view',
+      }
+      flashData.errors = [validationError]
+      flashData.formValues = [{ reference: missingReference }]
+
+      return request(app)
+        .get(url)
+        .expect('Content-Type', /html/)
+        .expect(res => {
+          const $ = cheerio.load(res.text)
+          expect($('.govuk-error-summary a[href=#reference-error]').text()).toBe(validationError.msg)
+          expect($('#reference-error').text()).toContain(validationError.msg)
+        })
+    })
+  })
+
+  describe(`POST ${url}`, () => {
+    beforeEach(() => {
+      flashData = { errors: [] }
+    })
+
+    it('should require booker admin role', () => {
+      app = appWithAllRoutes({ services: { bookerService }, sessionData })
+      return request(app).post(url).expect(302).expect('location', '/authError')
+    })
+
+    it('should redirect to the booker details page, for selected booker account', () => {
+      sessionData.matchedBookers = [booker1, booker2]
+
+      return request(app)
+        .post(url)
+        .send({ reference: booker1.reference })
+        .expect(302)
+        .expect('location', `/manage-bookers/${booker1.reference}/booker-details`)
+    })
+
+    it('should set form validation errors and redirect to same page', () => {
+      const expectedValidationError: FieldValidationError = {
+        location: 'body',
+        msg: 'Select an account to view',
+        path: 'reference',
+        type: 'field',
+        value: '',
+      }
+
+      return request(app)
+        .post(url)
+        .send({ reference: '' })
+        .expect(302)
+        .expect('location', url)
+        .expect(() => {
+          expect(flashProvider).toHaveBeenCalledWith('errors', [expectedValidationError])
+        })
+    })
+  })
+})
