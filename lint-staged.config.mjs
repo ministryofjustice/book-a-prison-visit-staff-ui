8adbd6ca0bec1df6a0e5810a9ce82c23c215ff80
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2025-11-04 11:59:44 +0000

    Fix lint-staged config to match 'rpm run lint' config (#1146)

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,4 @@
 export default {
-  '*.{ts,js,mjs,css}': ['prettier --write', 'eslint --fix'],
+  '*.{ts,js,mjs,css}': ['prettier --write', 'eslint --fix --max-warnings 0'],
   '*.json': ['prettier --write'],
 }
