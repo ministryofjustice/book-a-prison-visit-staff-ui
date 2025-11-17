c080bb30c304494df032010bda513b25f2b13b75
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2025-11-17 09:27:55 +0000

    force commit, WIP

--- server/routes/bookerManagement/bookerLinkVisitor.ts
+++ server/routes/bookerManagement/bookerLinkVisitor.ts
@@ -9,19 +9,41 @@ export default class BookerLinkVisitorController {
 
   public view(): RequestHandler {
     return async (req, res) => {
-      const { reference } = req.params
+      const { reference, prisonerId, visitorId } = req.params
+      const { username } = res.locals.user
 
-      // TODO render notify booker by email page
+      // duplication *necessary?* from bookerLinkVisitorListController
+      const booker = await this.bookerService.getBookerDetails({ username, reference })
+      const prisoner = booker.permittedPrisoners.find(
+        permittedPrisoner => permittedPrisoner.prisoner.prisonerNumber === prisonerId,
+      )?.prisoner
+
+      if (!prisoner) {
+        return res.redirect(`/manage-bookers/${reference}/booker-details`)
+      }
+
+      return res.render('pages/bookerManagement/bookerLinkVisitor', {
+        prisoner,
+        visitorId,
+      })
     }
   }
 
   public submit(): RequestHandler {
     return async (req, res) => {
-      const { reference } = req.params
+      const { reference, prisonerId, visitorId } = req.params
+      const { username } = res.locals.user
+      const { notifyBooker }: { notifyBooker: string } = req.body
 
-      // TODO call service to link visitor
-      // TODO HMPPS Audit
-      // TODO redirect to booker details with flash message
+      const permittedVisitor = await this.bookerService.linkBookerVisitor({
+        username,
+        reference,
+        prisonerId,
+        visitorId, // needs to be converted to number
+        notifyBooker,
+      })
+
+      return res.redirect(`/manage-bookers/${reference}/booker-details`)
     }
   }
 
