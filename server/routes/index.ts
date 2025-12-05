a0f19c3d42893d86e6db94ccdc155309fc189b1f
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2025-12-05 18:11:07 +0000

    VB-6224 Add visitor request feature flag and badge count (#1176)

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,24 +3,38 @@ import { Router } from 'express'
 import { clearSession } from './visitorUtils'
 import { Services } from '../services'
 import bapvUserRoles from '../constants/bapvUserRoles'
+import config from '../config'
 
-export default function routes({ visitNotificationsService, visitRequestsService }: Services): Router {
+export default function routes({ bookerService, visitNotificationsService, visitRequestsService }: Services): Router {
   const router = Router()
 
   router.get('/', async (req, res) => {
     const prison = req.session.selectedEstablishment
     const { username, userRoles } = res.locals.user
 
+    // Requested visits tile and count (public-enabled prisons only)
     const showRequestedVisitsTile = prison.isEnabledForPublic
-    const requestCount = showRequestedVisitsTile
+    const visitRequestCount = showRequestedVisitsTile
       ? (await visitRequestsService.getVisitRequestCount(username, prison.prisonId)).count
       : null
 
-    const reviewCount = (await visitNotificationsService.getNotificationCount(username, prison.prisonId)).count
+    // Visits needing review count (all prisons)
+    const visitReviewCount = (await visitNotificationsService.getNotificationCount(username, prison.prisonId)).count
 
+    // Manage online bookers tile (users with ADMIN role) and visitor requests count (pubic-enabled prisons only)
     const showBookerManagementTile = userRoles.includes(bapvUserRoles.BOOKER_ADMIN)
+    const visitorRequestCount =
+      config.features.visitorRequests.enabled && showBookerManagementTile && prison.isEnabledForPublic
+        ? await bookerService.getVisitorRequestCount({ username, prisonId: prison.prisonId })
+        : 0
 
-    res.render('pages/index', { showRequestedVisitsTile, requestCount, reviewCount, showBookerManagementTile })
+    res.render('pages/index', {
+      showRequestedVisitsTile,
+      visitRequestCount,
+      visitReviewCount,
+      showBookerManagementTile,
+      visitorRequestCount,
+    })
   })
 
   router.get('/back-to-start', (req, res) => {
