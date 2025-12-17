db8d6721a5ecea0c3cc518c715965a5cfe4de5de
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2025-12-17 17:21:15 +0000

    Get FE components when handling error for a POST request

--- server/errorHandler.ts
+++ server/errorHandler.ts
@@ -1,16 +1,31 @@
 import type { Request, Response, NextFunction } from 'express'
 import type { HTTPError } from 'superagent'
 import { SanitisedError } from '@ministryofjustice/hmpps-rest-client'
+import { getFrontendComponents } from '@ministryofjustice/hmpps-connect-dps-components'
 import logger from '../logger'
 import { errorHasStatus, getErrorStatus } from './utils/errorHelpers'
+import config from './config'
 
 export default function createErrorHandler(production: boolean) {
-  return (error: HTTPError | SanitisedError, req: Request, res: Response, next: NextFunction): void => {
+  return async (error: HTTPError | SanitisedError, req: Request, res: Response, next: NextFunction): Promise<void> => {
     logger.error(`Error handling request for '${req.originalUrl}', user '${res.locals.user?.username}'`, error)
 
     if (errorHasStatus(error, 401) || errorHasStatus(error, 403)) {
       logger.info('Logging user out')
       return res.redirect('/sign-out')
+    }
+
+    if (req.method === 'POST') {
+      try {
+        await getFrontendComponents({
+          logger,
+          componentApiConfig: config.apis.componentApi,
+          dpsUrl: config.dpsHome,
+          requestOptions: { includeSharedData: true },
+        })(req, res, next)
+      } catch (err) {
+        logger.error('Error fetching frontend components while handling failed POST request', err)
+      }
     }
 
     const heading = errorHasStatus(error, 404) ? 'Page not found' : 'Error'
