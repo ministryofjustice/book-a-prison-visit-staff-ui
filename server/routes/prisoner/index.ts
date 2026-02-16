cd8a666c682f842b750cca8ea8b832228b4ea9d1
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2026-02-16 11:02:49 +0000

    VB-6275, remove both VO feature flags

--- server/routes/prisoner/index.ts
+++ server/routes/prisoner/index.ts
@@ -3,7 +3,6 @@ import { BadRequest } from 'http-errors'
 import { Services } from '../../services'
 import VisitOrdersHistoryController from './visitOrders/visitOrdersHistoryController'
 import { isValidPrisonerNumber } from '../validationChecks'
-import config from '../../config'
 import PrisonerProfileController from './prisonerProfileController'
 import EditVisitOrdersBalancesController from './visitOrders/editVisitOrdersBalancesController'
 
@@ -34,17 +33,14 @@ export default function routes(services: Services): Router {
   router.post('/:prisonerId', prisonerProfileController.submit())
 
   // Visiting orders
-  if (config.features.voAdjustment.enabled) {
-    router.get('/:prisonerId/edit-visiting-orders-balances', editVisitOrdersBalancesController.view())
-    router.post(
-      '/:prisonerId/edit-visiting-orders-balances',
-      editVisitOrdersBalancesController.validate(),
-      editVisitOrdersBalancesController.submit(),
-    )
-  }
-  if (config.features.voHistory.enabled) {
-    router.get('/:prisonerId/visiting-orders-history', visitOrdersHistoryController.view())
-  }
+  router.get('/:prisonerId/edit-visiting-orders-balances', editVisitOrdersBalancesController.view())
+  router.post(
+    '/:prisonerId/edit-visiting-orders-balances',
+    editVisitOrdersBalancesController.validate(),
+    editVisitOrdersBalancesController.submit(),
+  )
+
+  router.get('/:prisonerId/visiting-orders-history', visitOrdersHistoryController.view())
 
   return router
 }
