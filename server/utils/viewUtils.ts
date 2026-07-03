d657e6715692f26f1e19510dcc19659a416ccf80
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2026-07-03 13:58:36 +0100

    Improve conversion to string

--- server/utils/viewUtils.ts
+++ server/utils/viewUtils.ts
@@ -5,6 +5,6 @@ const environment = new nunjucks.Environment()
 export default class ViewUtils {
   static escape(val: string): string {
     const escape = environment.getFilter('escape')
-    return escape(val).val
+    return String(escape(val))
   }
 }
