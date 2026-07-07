a9238e75c0516874f4b815efaf3c2cb08992effe
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2026-07-07 16:39:00 +0100

    Add temporary block type choice redirect

--- server/routes/blockDatesOrSessions/index.ts
+++ server/routes/blockDatesOrSessions/index.ts
@@ -3,6 +3,7 @@ import { Services } from '../../services'
 import BlockDatesOrSessionsController from './blockDatesOrSessionsController'
 import BlockDateController from './blockDates/blockDateController'
 import UnblockVisitDateController from './blockDates/unblockDateController'
+import ChooseDateOrSessionBlockController from './chooseDateOrSessionBlockController'
 
 export default function routes(services: Services): Router {
   const router = Router()
@@ -11,19 +12,31 @@ export default function routes(services: Services): Router {
     services.blockDatesOrSessionsService,
     services.visitSessionsService,
   )
+
+  const chooseDateOrSessionBlockController = new ChooseDateOrSessionBlockController(
+    services.auditService,
+    services.blockDatesOrSessionsService,
+  )
+
   const blockDateController = new BlockDateController(
     services.auditService,
     services.blockDatesOrSessionsService,
     services.visitService,
   )
+
   const unblockDateController = new UnblockVisitDateController(
     services.auditService,
     services.blockDatesOrSessionsService,
   )
 
+  // Block visit dates or sessions main page
   router.get('/', blockDatesOrSessionsController.view())
   router.post('/', blockDatesOrSessionsController.validate(), blockDatesOrSessionsController.submit())
 
+  // Choose whether to block a date or a session
+  router.get('/block-date-or-session', chooseDateOrSessionBlockController.view())
+
+  // Date block pages
   router.get('/block-new-date', blockDateController.view())
   router.post('/block-new-date', blockDateController.validate(), blockDateController.submit())
 
