c664f90e500a93c8cf55b43247deb0a12db7d995
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2026-09-18 20:09:49 +0100

    Fix following Copilot review

--- server/services/visitService.ts
+++ server/services/visitService.ts
@@ -183,7 +183,9 @@ export default class VisitService {
   private buildVisitorDetails(visitors: VisitorListItem[]): BookingRequestVisitorDetailsDto[] {
     return visitors.map(visitor => ({
       visitorId: visitor.personId,
-      visitorAge: visitor.age,
+      // TODO the '?? null' below is to handle in-progress sessions where age might not have been set earlier in the journey.
+      // Will be resolved within minutes of deployment then this can be removed.
+      visitorAge: visitor.age ?? null,
     }))
   }
 }
